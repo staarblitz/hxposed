8fbc33cfd7d6871e612d977d87f3426310c124e0
Staarblitz <vlltx@tuta.io>
2025-11-12 17:48:14 +0300

    Add error handling for -1

--- HxPosed.GUI/libhxposed/hxposed.c
+++ HxPosed.GUI/libhxposed/hxposed.c
@@ -4,11 +4,15 @@
 
 
 __declspec(dllexport) hypervisor_error_t get_hx_state(hx_status_response_t* resp) {
-	hypervisor_error_t err = { 0 };
 	hypervisor_req_resp_t req_resp = { 0 };
 	req_resp.call = call_get_status();
 
-	trap(&req_resp);
+	if (trap(&req_resp) == -1) {
+		hypervisor_error_t err = { 0 };
+		err.code = NotLoaded;
+		err.source = Hx;
+		return err;
+	}
 
 	resp->status = req_resp.arg1;
 	resp->version = req_resp.arg2;
