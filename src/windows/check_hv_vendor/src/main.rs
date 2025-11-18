d1cf9f50354aedef0f2004530c5428864e6e77b1
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-11-18 22:43:59 +0300

    Authorize first, get state then

--- src/windows/check_hv_vendor/src/main.rs
+++ src/windows/check_hv_vendor/src/main.rs
@@ -1,14 +1,35 @@
-use std::str::FromStr;
-use hxposed_core::hxposed::requests::Vmcall;
+use hxposed_core::error::HypervisorError;
+use hxposed_core::hxposed::call::HypervisorCall;
 use hxposed_core::hxposed::requests::auth::AuthorizationRequest;
 use hxposed_core::hxposed::requests::status::StatusRequest;
+use hxposed_core::hxposed::requests::{Vmcall, VmcallRequest};
 use hxposed_core::plugins::plugin_perms::PluginPermissions;
+use hxposed_core::services::process::HxProcess;
+use std::str::FromStr;
 use uuid::Uuid;
 
 fn main() {
-    println!("Preparing status request...");
+    let uuid = Uuid::from_str("ca170835-4a59-4c6d-a04b-f5866f592c38").unwrap();
+    println!("Authorizing with UUID {}", uuid);
+    let parts = uuid.as_u64_pair();
+    println!("Parts: {:x}, {:x}", parts.0, parts.1);
+    let req = AuthorizationRequest::from_raw(
+        HypervisorCall::default(),
+        (parts.0, parts.1, PluginPermissions::all().bits()),
+    );
+
+    let resp = match req.send() {
+        Err(e) => {
+            println!("Error authorization request! {:?}", e);
+            return;
+        }
+        Ok(x) => x,
+    };
+
+    println!("Permissions: {:?}", resp.permissions);
+
+    println!("Getting status");
     let req = StatusRequest::default();
-    println!("Status request: {:?}", req);
     let resp = req.send();
     match resp {
         Err(e) => {
@@ -24,22 +45,12 @@ fn main() {
         resp.state, resp.version
     );
 
-    let uuid = Uuid::from_str("ca170835-4a59-4c6d-a04b-f5866f592c38").unwrap();
-    println!("Authorizing with UUID {}", uuid);
-    let req = AuthorizationRequest {
-        permissions: PluginPermissions::all(),
-        uuid,
-    };
-
-    let resp = req.send();
-    match resp {
+    println!("Trying to open a process...");
+    let mut process = match HxProcess::open(4) {
+        Ok(x) => x,
         Err(e) => {
-            println!("Error authorization request! {:?}", e);
+            println!("Error opening process: {:?}", e);
+            return;
         }
-        _ => {}
-    }
-
-    let resp = unsafe { resp.unwrap_unchecked() };
-
-    println!("Permissions: {:?}", resp.permissions);
+    };
 }
