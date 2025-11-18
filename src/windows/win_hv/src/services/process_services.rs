8f7deea4dcd877c78d701823418e470d643f263b
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-11-18 22:45:08 +0300

    Return a result dispatcher can return to guest

--- src/windows/win_hv/src/services/process_services.rs
+++ src/windows/win_hv/src/services/process_services.rs
@@ -3,11 +3,14 @@ use crate::services::require_perm;
 use crate::write_response;
 use core::sync::atomic::{AtomicPtr, Ordering};
 use hv::hypervisor::host::Guest;
-use hxposed_core::hxposed::call::HypervisorCall;
+use hxposed_core::hxposed::call::{HypervisorCall, HypervisorResult};
 use hxposed_core::hxposed::error::NotAllowedReason;
+use hxposed_core::hxposed::func::ServiceFunction;
 use hxposed_core::hxposed::requests::VmcallRequest;
 use hxposed_core::hxposed::requests::process::{CloseProcessRequest, OpenProcessRequest};
-use hxposed_core::hxposed::responses::HypervisorResponse;
+use hxposed_core::hxposed::responses::empty::EmptyResponse;
+use hxposed_core::hxposed::responses::process::OpenProcessResponse;
+use hxposed_core::hxposed::responses::{HypervisorResponse, VmcallResponse};
 use hxposed_core::plugins::plugin_perms::PluginPermissions;
 use wdk_sys::ntddk::PsLookupProcessByProcessId;
 use wdk_sys::{PEPROCESS, STATUS_SUCCESS};
@@ -16,7 +19,7 @@ pub(crate) fn close_process(
     guest: &mut dyn Guest,
     request: CloseProcessRequest,
     plugin: &'static mut Plugin,
-) {
+) -> HypervisorResponse {
     if let Some((index, _)) = plugin
         .open_processes
         .iter()
@@ -24,13 +27,11 @@ pub(crate) fn close_process(
         .find(|(_, x)| x.load(Ordering::Relaxed).addr() as u64 == request.addr)
     {
         plugin.open_processes.remove(index);
+        EmptyResponse::with_service(ServiceFunction::CloseProcess)
     } else {
         // this is weird. a plugin should never attempt to close a process it has never opened in the first place.
         // abuse detected. blacklist the plugin (soon)
-        write_response(
-            guest,
-            HypervisorResponse::not_allowed(NotAllowedReason::Unknown, PluginPermissions::empty()),
-        );
+        HypervisorResponse::not_allowed(NotAllowedReason::Unknown, PluginPermissions::empty())
     }
 }
 
@@ -38,22 +39,26 @@ pub(crate) fn open_process(
     guest: &mut dyn Guest,
     request: OpenProcessRequest,
     plugin: &'static mut Plugin,
-) {
+) -> HypervisorResponse {
     if !require_perm(
         guest,
         plugin.permissions,
         PluginPermissions::PROCESS_EXECUTIVE,
     ) {
-        return;
+        return HypervisorResponse::not_allowed(
+            NotAllowedReason::MissingPermissions,
+            PluginPermissions::PROCESS_EXECUTIVE,
+        );
     }
     let mut process = PEPROCESS::default();
 
     let status = unsafe { PsLookupProcessByProcessId(request.process_id as _, &mut process) };
 
     if status != STATUS_SUCCESS {
-        write_response(guest, HypervisorResponse::nt_error(status as _));
-        return;
+        return HypervisorResponse::nt_error(status as _);
     }
 
-    plugin.open_processes.push(AtomicPtr::new(process))
+    plugin.open_processes.push(AtomicPtr::new(process));
+
+    OpenProcessResponse { addr: process as _ }.into_raw()
 }
