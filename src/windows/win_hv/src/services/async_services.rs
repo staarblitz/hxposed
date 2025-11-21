1d19b3f2475e6f48a40f82b03e84c86bac51b272
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-11-21 21:57:01 +0300

    Use mem::transmute

--- src/windows/win_hv/src/services/async_services.rs
+++ src/windows/win_hv/src/services/async_services.rs
@@ -1,12 +1,14 @@
+use crate::plugins::plugin::Plugin;
 use alloc::vec;
 use hv::hypervisor::host::Guest;
 use hxposed_core::hxposed::error::NotAllowedReason;
 use hxposed_core::hxposed::func::ServiceFunction;
-use hxposed_core::hxposed::requests::async_help::{AddAsyncHandlerRequest, RemoveAsyncHandlerRequest};
-use hxposed_core::hxposed::responses::empty::EmptyResponse;
+use hxposed_core::hxposed::requests::async_help::{
+    AddAsyncHandlerRequest, RemoveAsyncHandlerRequest,
+};
 use hxposed_core::hxposed::responses::HypervisorResponse;
+use hxposed_core::hxposed::responses::empty::EmptyResponse;
 use hxposed_core::services::async_service::AsyncNotifyHandler;
-use crate::plugins::plugin::Plugin;
 
 pub fn add_async_handler(
     guest: &mut dyn Guest,
@@ -20,21 +22,17 @@ pub fn add_async_handler(
     match plugin.add_notify_handler(AsyncNotifyHandler {
         filter: vec![],
         cookie: request.cookie,
-        handler: request.addr as *mut  _ as _
+        handler: unsafe { core::mem::transmute(request.addr) },
     }) {
-        Ok(_) => {
-            EmptyResponse::with_service(ServiceFunction::AddAsyncHandler)
-        }
-        Err(_) => {
-            HypervisorResponse::not_allowed(NotAllowedReason::Unknown)
-        }
+        Ok(_) => EmptyResponse::with_service(ServiceFunction::AddAsyncHandler),
+        Err(_) => HypervisorResponse::not_allowed(NotAllowedReason::Unknown),
     }
 }
 
 pub fn remove_async_handler(
     guest: &mut dyn Guest,
     request: RemoveAsyncHandlerRequest,
-    plugin: &'static mut Plugin
+    plugin: &'static mut Plugin,
 ) -> HypervisorResponse {
     plugin.remove_notify_handler(request.cookie);
 
