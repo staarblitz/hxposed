49047636ed847015c2d34964792c41842d7a4ac2
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-11-30 21:23:53 +0300

    Simplify

--- src/windows/win_hv/src/plugins/async_command.rs
+++ src/windows/win_hv/src/plugins/async_command.rs
@@ -8,7 +8,7 @@ use hxposed_core::hxposed::responses::HypervisorResponse;
 use hxposed_core::services::async_service::AsyncInfo;
 use wdk::println;
 use wdk_sys::ntddk::{ProbeForWrite, ZwSetEvent};
-use wdk_sys::{PEPROCESS, STATUS_SUCCESS};
+use wdk_sys::{HANDLE, PEPROCESS, STATUS_SUCCESS};
 
 pub trait AsyncCommand: Any {
     fn get_service_function(&self) -> ServiceFunction;
@@ -78,27 +78,11 @@ impl AsyncCommand for KillProcessAsyncCommand {
         // we are in context of system. we need to switch to context of plugin's process to get access to its handle table and virtual address space.
         let _ctx = ApcProcessContext::begin(self.plugin_process);
 
-        match microseh::try_seh(|| unsafe {
-            ProbeForWrite(self.info.result_values.load(Ordering::Relaxed) as _, 16, 1)
-        }) {
-            Ok(_) => {
-                let ptr = &mut unsafe { *self.info.result_values.load(Ordering::Relaxed) };
-                ptr[0] = result.result.into_bits() as u64;
-                ptr[1] = result.arg1;
-                ptr[2] = result.arg2;
-                ptr[3] = result.arg3;
-            }
-            Err(x) => {
-                println!("Failed to write to user buffer: {}", x);
-            }
-        }
-
-        match unsafe { ZwSetEvent(self.info.handle as _, Default::default()) } {
-            STATUS_SUCCESS => {}
-            err => {
-                println!("Failed to set event: {}", err);
-            }
-        }
+        write_and_set(
+            &result,
+            self.info.result_values.load(Ordering::Relaxed),
+            self.info.handle as _,
+        );
 
         drop(_ctx);
         // seems like our user mode app tried to be a little too smart.
@@ -116,32 +100,37 @@ impl AsyncCommand for GetProcessFieldAsyncCommand {
     fn complete(&mut self, result: HypervisorResponse) {
         let _ctx = ApcProcessContext::begin(self.plugin_process);
 
-        match microseh::try_seh(|| unsafe {
-            ProbeForWrite(self.info.result_values.load(Ordering::Relaxed) as _, 16, 1)
-        }) {
-            Ok(_) => {
-                let ptr = &mut unsafe { *self.info.result_values.load(Ordering::Relaxed) };
-                ptr[0] = result.result.into_bits() as u64;
-                ptr[1] = result.arg1;
-                ptr[2] = result.arg2;
-                ptr[3] = result.arg3;
-            }
-            Err(x) => {
-                println!("Failed to write to user buffer: {}", x);
-            }
-        }
-
-        match unsafe { ZwSetEvent(self.info.handle as _, Default::default()) } {
-            STATUS_SUCCESS => {}
-            err => {
-                println!("Failed to set event: {}", err);
-            }
-        }
+        write_and_set(
+            &result,
+            self.info.result_values.load(Ordering::Relaxed),
+            self.info.handle as _,
+        );
 
         drop(_ctx);
     }
 
     fn as_any(&self) -> &dyn Any {
         self
+    }
+}
+
+fn write_and_set(result: &HypervisorResponse, result_values: *mut u64, handle: HANDLE) {
+    match microseh::try_seh(|| unsafe { ProbeForWrite(result_values as _, 16, 1) }) {
+        Ok(_) => unsafe {
+            result_values.write(result.result.into_bits() as u64);
+            result_values.offset(1).write(result.arg1);
+            result_values.offset(2).write(result.arg2);
+            result_values.offset(3).write(result.arg3);
+        }
+        Err(x) => {
+            println!("Failed to write to user buffer: {}", x);
+        }
+    }
+
+    match unsafe { ZwSetEvent(handle as _, Default::default()) } {
+        STATUS_SUCCESS => {}
+        err => {
+            println!("Failed to set event: {}", err);
+        }
     }
 }
