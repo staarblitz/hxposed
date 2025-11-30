a451fb9ed1e3aa8bda538e027c6ac639bebb002b
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-11-30 12:14:20 +0300

    Import functions from kernel32

---
+++ src/hxposed_core/src/intern/win.rs
@@ -0,0 +1,12 @@
+#[cfg(feature = "usermode")]
+#[link(name = "kernel32.dll")]
+unsafe extern "C" {
+    #[allow(non_snake_case)]
+    pub(crate) fn WaitForSingleObject(handle: u64, time: u32) -> u32;
+
+    #[allow(non_snake_case)]
+    pub(crate) fn CreateEventA(security_attributes: *mut u8, manual_reset: u32, initial_state: u32, name: *mut u8) -> u64;
+
+    #[allow(non_snake_case)]
+    pub(crate) fn CloseHandle(handle: u64);
+}
