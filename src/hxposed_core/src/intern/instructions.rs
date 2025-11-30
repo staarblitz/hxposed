9efaaab33e841bf8ad37497e8b6df325a1d5b2eb
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-11-30 12:40:07 +0300

    Use Arc

--- src/hxposed_core/src/intern/instructions.rs
+++ src/hxposed_core/src/intern/instructions.rs
@@ -5,6 +5,7 @@ use crate::hxposed::error::{ErrorCode, ErrorSource};
 use crate::hxposed::requests::{HypervisorRequest, VmcallRequest};
 use crate::hxposed::responses::{HypervisorResponse, VmcallResponse};
 use core::arch::asm;
+use core::sync::atomic::Ordering;
 
 pub fn vmcall_typed<R: VmcallRequest>(req: R) -> Result<R::Response, HypervisorError> {
     let raw_resp = vmcall(req.into_raw());
@@ -21,7 +22,7 @@ pub(crate) fn vmcall(request: HypervisorRequest) -> HypervisorResponse {
         inout("r9") request.arg2 => response.arg2,
         inout("r10") request.arg3 => response.arg3,
         in("r11") request.async_handle,
-        in("r12") Box::leak(request.result_data) as *mut _,
+        in("r12") request.result_data.load(Ordering::Relaxed),
         inout("rsi") request.call.into_bits() => result,
         inout("rcx") leaf);
     }
