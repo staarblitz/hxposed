1e5687929166f94e27a15464ac75f886bc1b679e
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-11-30 21:21:51 +0300

    Add extra error information

--- src/hxposed_core/src/error.rs
+++ src/hxposed_core/src/error.rs
@@ -1,12 +1,14 @@
 use crate::hxposed::call::HypervisorResult;
 use crate::hxposed::error::{ErrorSource, InternalErrorCode};
 use static_assertions::assert_eq_size;
+use crate::hxposed::responses::HypervisorResponse;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Default)]
 #[repr(C)]
 pub struct HypervisorError {
     pub error_source: ErrorSource,
-    pub error_code: u32,
+    pub error_code: u16,
+    pub error_reason: u16
 }
 assert_eq_size!(HypervisorError, u64);
 
@@ -15,6 +17,7 @@ impl HypervisorError {
         Self {
             error_source: ErrorSource::Hx,
             error_code: InternalErrorCode::Ok as _,
+            error_reason: InternalErrorCode::Ok as _
         }
     }
 
@@ -22,19 +25,19 @@ impl HypervisorError {
         Self {
             error_source: ErrorSource::Hx,
             error_code: InternalErrorCode::NotFound as _,
+            error_reason: InternalErrorCode::NotFound as _
         }
     }
 
     pub fn is_err(&self) -> bool {
         !(self.error_code == InternalErrorCode::Ok as _ && self.error_source == ErrorSource::Hx)
     }
-}
 
-impl From<HypervisorResult> for HypervisorError {
-    fn from(value: HypervisorResult) -> Self {
+    pub fn from_response(response: HypervisorResponse) -> HypervisorError {
         Self {
-            error_source: value.error_source(),
-            error_code: value.error_code() as _,
+            error_source: response.result.error_source(),
+            error_code: response.result.error_code() as _,
+            error_reason: response.arg1 as _
         }
     }
 }
