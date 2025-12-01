871b299198bbf0a973de8d84d193106156e4a2e4
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-01 18:29:23 +0300

    Fix ownership semantics.

--- src/hxposed_core/src/services/async_service.rs
+++ src/hxposed_core/src/services/async_service.rs
@@ -3,6 +3,8 @@ use crate::hxposed::call::HypervisorResult;
 use crate::hxposed::error::InternalErrorCode;
 use crate::hxposed::responses::{HypervisorResponse, VmcallResponse};
 use alloc::boxed::Box;
+use alloc::sync::Arc;
+use alloc::vec::Vec;
 use core::marker::PhantomData;
 use core::ptr::null_mut;
 use core::sync::atomic::{AtomicPtr, Ordering};
@@ -26,25 +28,22 @@ pub static GLOBAL_ASYNC_NOTIFY_HANDLER: HxPosedAsyncService = HxPosedAsyncServic
 pub struct HxPosedAsyncService {}
 
 #[derive(Default, Debug)]
+pub struct UnsafeAsyncInfo {
+    pub handle: u64,
+    pub result_values: *mut [u64;4]
+}
+#[derive(Default, Debug)]
 pub struct AsyncInfo {
     pub handle: u64,
     // memory is manually managed, unfortunately
-    pub result_values: AtomicPtr<u64>,
-}
-
-impl Clone for AsyncInfo {
-    fn clone(&self) -> Self {
-        Self {
-            handle: self.handle,
-            result_values: AtomicPtr::new(self.result_values.load(Ordering::Relaxed)),
-        }
-    }
+    pub result_values: Box<[u64;4]>,
 }
 
 #[cfg(feature = "usermode")]
-#[derive(Clone, Debug, Default)]
+#[derive(Debug, Default)]
 pub struct AsyncPromise<T> {
     pub request: HypervisorRequest,
+    pub async_info: AsyncInfo,
     pub completed: bool,
     phantom: PhantomData<T>,
 }
@@ -53,20 +52,20 @@ impl<T> AsyncPromise<T>
 where
     T: VmcallResponse,
 {
-    pub fn send_async(&self) {
-        let response = vmcall(self.request.clone());
+    pub fn send_async(&mut self) {
+        let response = vmcall(self.request.clone(), Some(&mut self.async_info));
         if response.result.is_error() {
             unsafe {
                 let ptr = self
-                    .request
+
                     .async_info
-                    .result_values
-                    .load(Ordering::Relaxed);
+                    .result_values.as_mut_ptr();
+
                 ptr.write(response.result.into_bits() as _);
                 ptr.offset(1).write(response.arg1);
                 ptr.offset(2).write(response.arg2);
                 ptr.offset(3).write(response.arg3);
-                SetEvent(self.request.async_info.handle);
+                SetEvent(self.async_info.handle);
             }
         }
     }
@@ -82,25 +81,24 @@ where
     ///
     /// ## Return
     /// - [R`esult`] with the [`VmcallResponse`] on [`T`]
-    pub fn wait(self) -> Result<T, HypervisorError> {
-        unsafe { WaitForSingleObject(self.request.async_info.handle, u32::MAX) };
+    pub fn wait(mut self) -> Result<T, HypervisorError> {
+        unsafe { WaitForSingleObject(self.async_info.handle, u32::MAX) };
 
         let ptr = self
-            .request
             .async_info
-            .result_values
-            .load(Ordering::Relaxed);
+            .result_values.as_mut_ptr();
+
         let result = unsafe {
             T::from_raw(HypervisorResponse {
                 result: HypervisorResult::from_bits(ptr.read() as _),
                 arg1: ptr.offset(1).read() as _,
-                arg2: ptr.offset(1).read() as _,
-                arg3: ptr.offset(1).read() as _,
+                arg2: ptr.offset(2).read() as _,
+                arg3: ptr.offset(3).read() as _,
             })
         };
 
         unsafe {
-            CloseHandle(self.request.async_info.handle);
+            CloseHandle(self.async_info.handle);
         }
 
         result
@@ -119,14 +117,14 @@ where
     /// ## Return
     /// * [`InternalErrorCode::NotFound`] - Timed out.
     /// * [`T`] - Hypervisor responded.
-    pub fn wait_timespan(self, ms: u32) -> Result<T, HypervisorError> {
-        let result = match unsafe { WaitForSingleObject(self.request.async_info.handle, ms) } {
+    pub fn wait_timespan(mut self, ms: u32) -> Result<T, HypervisorError> {
+        let result = match unsafe { WaitForSingleObject(self.async_info.handle, ms) } {
             0 => unsafe {
                 let ptr = self
-                    .request
+
                     .async_info
-                    .result_values
-                    .load(Ordering::Relaxed);
+                    .result_values.as_mut_ptr();
+
                 T::from_raw(HypervisorResponse {
                     result: HypervisorResult::from_bits(ptr.read() as _),
                     arg1: ptr.offset(1).read() as _,
@@ -140,7 +138,7 @@ where
         };
 
         unsafe {
-            CloseHandle(self.request.async_info.handle);
+            CloseHandle(self.async_info.handle);
         }
 
         result
@@ -153,15 +151,15 @@ impl HxPosedAsyncService {
         T: VmcallResponse,
     {
         request.call.set_is_async(true);
-        request.async_info.handle = unsafe { CreateEventA(null_mut(), 0, 0, null_mut()) };
-        request.async_info.result_values = AtomicPtr::new(unsafe {
-            Box::leak(Box::<[u64; 4]>::new_uninit().assume_init()) as *mut _ as *mut u64
-        });
 
         Box::new(AsyncPromise::<T> {
             request,
             completed: false,
             phantom: PhantomData,
+            async_info:  AsyncInfo {
+                handle: unsafe { CreateEventA(null_mut(), 0, 0, null_mut()) },
+                result_values: Box::new([0;4])
+            }
         })
     }
 
