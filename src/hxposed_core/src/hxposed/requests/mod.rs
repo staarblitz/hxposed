4559a303b47cb8df1907498b86f19c20e397e50e
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-01 18:28:56 +0300

    Add send_async back. Move async_info from HypervisorRequest

--- src/hxposed_core/src/hxposed/requests/mod.rs
+++ src/hxposed_core/src/hxposed/requests/mod.rs
@@ -1,50 +1,29 @@
-use alloc::boxed::Box;
-use alloc::sync::Arc;
-use core::any::{Any, TypeId};
-use core::sync::atomic::{AtomicPtr, Ordering};
 use crate::error::HypervisorError;
 use crate::hxposed::call::HypervisorCall;
 use crate::hxposed::responses::VmcallResponse;
 use crate::intern::instructions::vmcall;
 #[cfg(feature = "usermode")]
-use crate::services::async_service::{AsyncPromise, GLOBAL_ASYNC_NOTIFY_HANDLER};
-use crate::services::async_service::{AsyncInfo, HxPosedAsyncService};
+use crate::services::async_service::AsyncPromise;
+use crate::services::async_service::HxPosedAsyncService;
+use alloc::boxed::Box;
+use core::any::Any;
 
 pub mod auth;
 pub mod process;
 pub mod status;
 
-#[derive(Default, Debug)]
+#[derive(Clone, Default, Debug)]
 pub struct HypervisorRequest {
     pub call: HypervisorCall,
     pub arg1: u64,
     pub arg2: u64,
     pub arg3: u64,
 
-    pub async_info: AsyncInfo,
-
     pub extended_arg1: u128,
     pub extended_arg2: u128,
     pub extended_arg3: u128,
     pub extended_arg4: u128,
 }
-
-impl Clone for HypervisorRequest {
-    fn clone(&self) -> Self {
-        Self {
-            call: self.call.clone(),
-            arg1: self.arg1.clone(),
-            arg2: self.arg2.clone(),
-            arg3: self.arg3.clone(),
-            async_info: self.async_info.clone(),
-            extended_arg1: self.extended_arg1,
-            extended_arg2: self.extended_arg2,
-            extended_arg3: self.extended_arg3,
-            extended_arg4: self.extended_arg4
-        }
-    }
-}
-
 pub trait VmcallRequest {
     type Response: VmcallResponse + Any + Send + Sync + Clone;
     fn into_raw(self) -> HypervisorRequest;
@@ -54,7 +33,9 @@ pub trait VmcallRequest {
 pub trait Vmcall<T: VmcallRequest> {
     fn send(self) -> Result<T::Response, HypervisorError>;
     #[cfg(feature = "usermode")]
-    fn get_promise(self) ->Box<AsyncPromise<T::Response>>;
+    fn send_async(self) -> Box<AsyncPromise<T::Response>>;
+    #[cfg(feature = "usermode")]
+    fn get_promise(self) -> Box<AsyncPromise<T::Response>>;
 }
 
 impl<T> Vmcall<T> for T
@@ -62,11 +43,20 @@ where
     T: VmcallRequest,
 {
     fn send(self) -> Result<T::Response, HypervisorError> {
-        T::Response::from_raw(vmcall(self.into_raw()))
+        T::Response::from_raw(vmcall(self.into_raw(), None))
+    }
+
+    #[cfg(feature = "usermode")]
+    fn send_async(self) -> Box<AsyncPromise<T::Response>> {
+        let mut promise = self.get_promise();
+
+        promise.send_async();
+
+        promise
     }
 
     #[cfg(feature = "usermode")]
     fn get_promise(self) -> Box<AsyncPromise<T::Response>> {
-       HxPosedAsyncService::new_promise(self.into_raw())
+        HxPosedAsyncService::new_promise(self.into_raw())
     }
 }
