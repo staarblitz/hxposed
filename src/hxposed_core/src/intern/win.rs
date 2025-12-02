71e741cbc76631739d9a9f6873173c5ed3603c53
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-02 11:40:22 +0300

    Add CreateThread

--- src/hxposed_core/src/intern/win.rs
+++ src/hxposed_core/src/intern/win.rs
@@ -1,3 +1,5 @@
+use core::ffi::c_void;
+
 #[cfg(feature = "usermode")]
 #[link(name = "kernel32")]
 unsafe extern "C" {
@@ -5,11 +7,26 @@ unsafe extern "C" {
     pub(crate) fn WaitForSingleObject(handle: u64, time: u32) -> u32;
 
     #[allow(non_snake_case)]
-    pub(crate) fn CreateEventA(security_attributes: *mut u8, manual_reset: u32, initial_state: u32, name: *mut u8) -> u64;
+    pub(crate) fn CreateEventA(
+        security_attributes: *mut u8,
+        manual_reset: u32,
+        initial_state: u32,
+        name: *mut u8,
+    ) -> u64;
 
     #[allow(non_snake_case)]
     pub(crate) fn CloseHandle(handle: u64);
 
     #[allow(non_snake_case)]
     pub(crate) fn SetEvent(handle: u64) -> u32;
+
+    #[allow(non_snake_case)]
+    pub(crate) fn CreateThread(
+        security_attributes: *mut u8,
+        stack_size: usize,
+        start_address: unsafe extern "C" fn(*mut u64) -> u32,
+        parameter: *mut u64,
+        creation_flags: u32,
+        thread_id: *mut u32,
+    ) -> u64;
 }
