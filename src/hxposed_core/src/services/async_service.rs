46cc4c5f12c4da63f450bf0fcf279275663ead78
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-02 14:10:43 +0300

    Add is_present

--- src/hxposed_core/src/services/async_service.rs
+++ src/hxposed_core/src/services/async_service.rs
@@ -28,6 +28,13 @@ pub struct UnsafeAsyncInfo {
     pub handle: u64,
     pub result_values: *mut [u64; 4],
 }
+
+impl UnsafeAsyncInfo {
+    pub fn is_present(&self) -> bool {
+        self.handle != 0
+    }
+}
+
 #[derive(Default, Debug)]
 pub struct AsyncInfo {
     pub handle: u64,
