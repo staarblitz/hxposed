a3e9e3d4380bd3ee56ca6956a2e6473a0c5ea6aa
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-05 18:39:16 +0300

    Import GetCurrentProcessId

--- src/hxposed_core/src/intern/win.rs
+++ src/hxposed_core/src/intern/win.rs
@@ -7,6 +7,9 @@ unsafe extern "C" {
     pub(crate) fn WaitForSingleObject(handle: u64, time: u32) -> u32;
 
     #[allow(non_snake_case)]
+    pub(crate) fn GetCurrentProcessId() -> u32;
+
+    #[allow(non_snake_case)]
     pub(crate) fn CreateEventA(
         security_attributes: *mut u8,
         manual_reset: u32,
