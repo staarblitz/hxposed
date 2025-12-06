d3a8eb97728ab9ef0763b2851703b14727e6ce4a
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-06 16:49:03 +0300

    Move memory operations to their own file

--- src/hxposed_core/src/hxposed/requests/process.rs
+++ src/hxposed_core/src/hxposed/requests/process.rs
@@ -46,23 +46,7 @@ pub struct SetProcessFieldRequest {
     pub data_len: usize,
 }
 
-#[derive(Default, Debug)]
-#[repr(C)]
-pub struct RWProcessMemoryRequest {
-    pub id: u32,
-    pub address: *mut u8,
-    pub count: usize,
-    pub data: *mut u8,
-    pub data_len: usize,
-    pub operation: ProcessMemoryOperation,
-}
 
-#[derive(Default, Debug)]
-pub struct ProtectProcessMemoryRequest {
-    pub id: u32,
-    pub address: *mut u8,
-    pub protection: MemoryProtection,
-}
 
 impl VmcallRequest for OpenProcessRequest {
     type Response = OpenProcessResponse;
@@ -196,67 +180,7 @@ impl VmcallRequest for SetProcessFieldRequest {
     }
 }
 
-impl VmcallRequest for RWProcessMemoryRequest {
-    type Response = RWProcessMemoryResponse;
 
-    fn into_raw(self) -> *mut HypervisorRequest {
-        let raw = Box::new(HypervisorRequest {
-            call: HypervisorCall::process_vm_op(),
-            arg1: self.id as _,
-            arg2: self.address as _,
-            arg3: self.count as _,
-
-            extended_arg1: self.data as _,
-            extended_arg2: self.data_len as _,
-            extended_arg3: self.operation.clone().into_bits() as _,
-
-            ..Default::default()
-        });
-
-        mem::forget(self);
-
-        Box::into_raw(raw)
-    }
-
-    fn from_raw(request: &HypervisorRequest) -> Self {
-        Self {
-            id: request.arg1 as _,
-            address: request.arg2 as *mut u8,
-            count: request.arg3 as _,
-
-            data: request.extended_arg1 as _,
-            data_len: request.extended_arg2 as _,
-            operation: ProcessMemoryOperation::from_bits(request.extended_arg3 as _),
-        }
-    }
-}
-
-impl VmcallRequest for ProtectProcessMemoryRequest {
-    type Response = ProtectProcessMemoryResponse;
-
-    fn into_raw(self) -> *mut HypervisorRequest {
-        let raw = Box::new(HypervisorRequest {
-            call: HypervisorCall::process_vm_protect(),
-            arg1: self.id as _,
-            arg2: self.address as _,
-            arg3: self.protection.bits() as _,
-
-            ..Default::default()
-        });
-
-        mem::forget(self);
-
-        Box::into_raw(raw)
-    }
-
-    fn from_raw(request: &HypervisorRequest) -> Self {
-        Self {
-            id: request.arg1 as _,
-            address: request.arg2 as _,
-            protection: MemoryProtection::from_bits(request.arg3 as _).unwrap(),
-        }
-    }
-}
 
 impl SetProcessFieldRequest {
     pub(crate) fn set_protection(id: u32, new_protection: &mut ProcessProtection) -> Self {
@@ -274,27 +198,6 @@ impl SetProcessFieldRequest {
             field: ProcessField::Signers,
             data: new_levels as *mut _ as *mut u8,
             data_len: size_of::<ProcessSignatureLevels>() as _,
-        }
-    }
-}
-
-#[derive(Clone, Default, Eq, PartialEq, Hash, Debug)]
-pub enum ProcessMemoryOperation {
-    #[default]
-    Read,
-    Write,
-}
-
-impl ProcessMemoryOperation {
-    pub const fn into_bits(self) -> u8 {
-        self as _
-    }
-
-    pub const fn from_bits(bits: u8) -> Self {
-        match bits {
-            0 => Self::Read,
-            1 => Self::Write,
-            _ => Self::Read,
         }
     }
 }
