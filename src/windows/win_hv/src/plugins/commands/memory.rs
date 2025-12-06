2fb0400d85bbe1b3db483d0d46ca4e8b3dfb030e
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-06 16:50:59 +0300

    Add new memory async commands

--- src/windows/win_hv/src/plugins/commands/memory.rs
+++ src/windows/win_hv/src/plugins/commands/memory.rs
@@ -1,11 +1,10 @@
 use crate::plugins::commands::{AsyncCommand, write_and_set};
 use core::any::Any;
 use hxposed_core::hxposed::func::ServiceFunction;
-use hxposed_core::hxposed::requests::process::{
-    ProtectProcessMemoryRequest, RWProcessMemoryRequest,
-};
+use hxposed_core::hxposed::requests::memory::*;
 use hxposed_core::hxposed::responses::HypervisorResponse;
 use hxposed_core::services::async_service::UnsafeAsyncInfo;
+use uuid::Uuid;
 use wdk_sys::PEPROCESS;
 
 pub struct RWProcessMemoryAsyncCommand {
@@ -20,6 +19,78 @@ pub struct ProtectProcessMemoryAsyncCommand {
     pub process: PEPROCESS,
     pub command: ProtectProcessMemoryRequest,
     pub async_info: UnsafeAsyncInfo,
+}
+
+pub struct AllocateMemoryAsyncCommand {
+    pub command: AllocateMemoryRequest,
+    pub uuid: Uuid,
+    pub async_info: UnsafeAsyncInfo,
+}
+
+pub struct MapMemoryAsyncCommand {
+    pub command: MapMemoryRequest,
+    pub uuid: Uuid,
+    pub async_info: UnsafeAsyncInfo,
+}
+
+pub struct FreeMemoryAsyncCommand {
+    pub command: FreeMemoryRequest,
+    pub uuid: Uuid,
+    pub async_info: UnsafeAsyncInfo,
+}
+
+impl AsyncCommand for FreeMemoryAsyncCommand {
+    fn get_service_function(&self) -> ServiceFunction {
+        ServiceFunction::FreeMemory
+    }
+
+    fn complete(&mut self, result: HypervisorResponse) {
+        write_and_set(
+            &result,
+            self.async_info.result_values as *mut _,
+            self.async_info.handle as _,
+        )
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl AsyncCommand for MapMemoryAsyncCommand {
+    fn get_service_function(&self) -> ServiceFunction {
+        ServiceFunction::MapMemory
+    }
+
+    fn complete(&mut self, result: HypervisorResponse) {
+        write_and_set(
+            &result,
+            self.async_info.result_values as *mut _,
+            self.async_info.handle as _,
+        )
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl AsyncCommand for AllocateMemoryAsyncCommand {
+    fn get_service_function(&self) -> ServiceFunction {
+        ServiceFunction::AllocateMemory
+    }
+
+    fn complete(&mut self, result: HypervisorResponse) {
+        write_and_set(
+            &result,
+            self.async_info.result_values as *mut _,
+            self.async_info.handle as _,
+        )
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
 }
 
 impl AsyncCommand for ProtectProcessMemoryAsyncCommand {
