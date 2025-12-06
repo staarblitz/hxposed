9429ca85bf60ad67547368830a4f2713715e581b
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-06 12:24:57 +0300

    Add ProtectProcessMemoryResponse

--- src/hxposed_core/src/hxposed/responses/process.rs
+++ src/hxposed_core/src/hxposed/responses/process.rs
@@ -2,6 +2,7 @@ use crate::error::HypervisorError;
 use crate::hxposed::call::HypervisorResult;
 use crate::hxposed::func::ServiceFunction;
 use crate::hxposed::responses::{HypervisorResponse, VmcallResponse};
+use crate::services::types::memory_fields::MemoryProtection;
 
 #[derive(Clone, Default, Debug)]
 #[repr(C)]
@@ -16,12 +17,44 @@ pub enum GetProcessFieldResponse {
     Unknown = 0,
     NtPath(u16) = 1,
     Protection(u32) = 2,
-    Signers(u16) = 3
+    Signers(u16) = 3,
 }
 
-#[derive(Clone,Default,Debug)]
+#[derive(Clone, Default, Debug)]
 pub struct RWProcessMemoryResponse {
     pub bytes_processed: usize,
+}
+
+#[derive(Clone, Default, Debug)]
+pub struct ProtectProcessMemoryResponse {
+    pub old_protection: MemoryProtection,
+    pub base_address: u64,
+    pub bytes_processed: usize,
+}
+
+impl VmcallResponse for ProtectProcessMemoryResponse {
+    fn from_raw(raw: HypervisorResponse) -> Result<Self, HypervisorError> {
+        if raw.result.is_error() {
+            Err(HypervisorError::from_response(raw))
+        } else {
+            Ok(Self {
+                old_protection: MemoryProtection::from_bits(raw.arg1 as _).unwrap(),
+                base_address: raw.arg2 as _,
+                bytes_processed: raw.arg3 as _,
+            })
+        }
+    }
+
+    fn into_raw(self) -> HypervisorResponse {
+        HypervisorResponse {
+            result: HypervisorResult::ok(ServiceFunction::ProtectProcessMemory),
+            arg1: self.old_protection.bits() as _,
+            arg2: self.base_address as _,
+            arg3: self.bytes_processed as _,
+
+            ..Default::default()
+        }
+    }
 }
 
 impl VmcallResponse for RWProcessMemoryResponse {
@@ -30,7 +63,7 @@ impl VmcallResponse for RWProcessMemoryResponse {
             Err(HypervisorError::from_response(raw))
         } else {
             Ok(Self {
-                bytes_processed: raw.arg1 as _
+                bytes_processed: raw.arg1 as _,
             })
         }
     }
@@ -55,7 +88,7 @@ impl VmcallResponse for GetProcessFieldResponse {
             1 => Self::NtPath(raw.arg2 as _),
             2 => Self::Protection(raw.arg2 as _),
             3 => Self::Signers(raw.arg2 as _),
-            _ => unreachable!("Developer forgot to implement this one.")
+            _ => unreachable!("Developer forgot to implement this one."),
         })
     }
 
