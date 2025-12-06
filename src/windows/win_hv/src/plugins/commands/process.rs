7a80f8cf9c1d2822f7d2f21fd2bcd7e56de0104d
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-06 19:48:48 +0300

    Add OpenProcessAsyncCommand and GetProcessThreadsAsyncCommand

--- src/windows/win_hv/src/plugins/commands/process.rs
+++ src/windows/win_hv/src/plugins/commands/process.rs
@@ -1,12 +1,10 @@
 use crate::plugins::commands::{write_and_set, AsyncCommand};
 use core::any::Any;
 use hxposed_core::hxposed::func::ServiceFunction;
-use hxposed_core::hxposed::requests::process::{
-    GetProcessFieldRequest, KillProcessRequest,
-    SetProcessFieldRequest,
-};
+use hxposed_core::hxposed::requests::process::{GetProcessFieldRequest, GetProcessThreadsRequest, KillProcessRequest, OpenProcessRequest, SetProcessFieldRequest};
 use hxposed_core::hxposed::responses::HypervisorResponse;
 use hxposed_core::services::async_service::UnsafeAsyncInfo;
+use uuid::Uuid;
 use wdk_sys::PEPROCESS;
 
 pub struct GetProcessFieldAsyncCommand {
@@ -28,6 +26,54 @@ pub struct KillProcessAsyncCommand {
     pub process: PEPROCESS,
     pub async_info: UnsafeAsyncInfo,
     pub plugin_process: PEPROCESS,
+}
+
+pub struct OpenProcessAsyncCommand {
+    pub command: OpenProcessRequest,
+    pub async_info: UnsafeAsyncInfo,
+    pub uuid: Uuid
+}
+
+pub struct GetProcessThreadsAsyncCommand {
+    pub process: PEPROCESS,
+    pub command: GetProcessThreadsRequest,
+    pub async_info: UnsafeAsyncInfo,
+}
+
+impl AsyncCommand for GetProcessThreadsAsyncCommand {
+    fn get_service_function(&self) -> ServiceFunction {
+        ServiceFunction::GetProcessThreads
+    }
+
+    fn complete(&mut self, result: HypervisorResponse) {
+        write_and_set(
+            &result,
+            self.async_info.result_values as *mut _,
+            self.async_info.handle as _,
+        );
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl AsyncCommand for OpenProcessAsyncCommand {
+    fn get_service_function(&self) -> ServiceFunction {
+        ServiceFunction::OpenProcess
+    }
+
+    fn complete(&mut self, result: HypervisorResponse) {
+        write_and_set(
+            &result,
+            self.async_info.result_values as *mut _,
+            self.async_info.handle as _,
+        );
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
 }
 
 impl AsyncCommand for KillProcessAsyncCommand {
