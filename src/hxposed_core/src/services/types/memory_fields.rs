33344ff9bdd377ecc1f3a20f167edc60bafbd783
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-06 12:25:28 +0300

    Reimplement as bitflags

--- src/hxposed_core/src/services/types/memory_fields.rs
+++ src/hxposed_core/src/services/types/memory_fields.rs
@@ -1,13 +1,21 @@
-#[derive(Copy, Clone, Default, Debug)]
-#[repr(u32)]
-pub enum MemoryProtection {
-    #[default]
-    NoAccess = 0x1,
-    ReadOnly = 0x2,
-    ReadWrite = 0x4,
-    WriteCopy = 0x8,
-    Execute = 0x10,
-    ExecuteRead = 0x20,
-    ExecuteReadWrite = 0x40,
-    Invalid = 0x40000000,
+use bitflags::bitflags;
+
+bitflags! {
+    #[derive(Default, Debug, Clone, Copy)]
+    #[repr(transparent)]
+    pub struct MemoryProtection : u32 {
+        const NONE = 0;
+        const NO_ACCESS = 0x1;
+        const READONLY = 0x2;
+        const READWRITE = 0x4;
+        const WRITECOPY = 0x8;
+        const EXECUTE  = 0x10;
+        const EXECUTE_READ = 0x20;
+        const EXECUTE_READWRITE = 0x40;
+        const EXECUTE_WRITECOPY = 0x80;
+        const GUARD = 0x100;
+        const NO_CACHE = 0x200;
+        const WRITE_COMBINE = 0x400;
+        const INVALID = 0x40000000;
+    }
 }
