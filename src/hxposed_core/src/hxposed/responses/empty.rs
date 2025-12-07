a6d3736c56fc5d9bc7ab6e7cd07334f99ff9838e
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-07 15:54:49 +0300

    Move OpenObjectResponse

--- src/hxposed_core/src/hxposed/responses/empty.rs
+++ src/hxposed_core/src/hxposed/responses/empty.rs
@@ -6,6 +6,29 @@ use crate::hxposed::responses::{HypervisorResponse, VmcallResponse};
 #[derive(Clone, Debug)]
 pub struct EmptyResponse;
 
+#[derive(Clone, Default, Debug)]
+pub struct OpenObjectResponse {
+    pub addr: u64,
+}
+
+impl VmcallResponse for OpenObjectResponse {
+    fn from_raw(raw: HypervisorResponse) -> Result<Self, HypervisorError> {
+        if raw.result.is_error() {
+            return Err(HypervisorError::from_response(raw));
+        }
+        Ok(Self { addr: raw.arg1 })
+    }
+
+    fn into_raw(self) -> HypervisorResponse {
+        HypervisorResponse {
+            result: HypervisorResult::ok(ServiceFunction::OpenProcess),
+            arg1: self.addr,
+            ..Default::default()
+        }
+    }
+}
+
+
 impl EmptyResponse{
     pub fn with_service(service_function: ServiceFunction) -> HypervisorResponse {
         HypervisorResponse{
