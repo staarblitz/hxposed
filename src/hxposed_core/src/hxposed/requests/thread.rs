244c3480d140d11b81b024f1a77b1d7e71a3182e
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-07 15:54:40 +0300

    Add new thread request module

---
+++ src/hxposed_core/src/hxposed/requests/thread.rs
@@ -0,0 +1,90 @@
+use crate::hxposed::call::HypervisorCall;
+use crate::hxposed::requests::process::ObjectOpenType;
+use crate::hxposed::requests::{HypervisorRequest, VmcallRequest};
+use crate::hxposed::responses::empty::{EmptyResponse, OpenObjectResponse};
+use alloc::boxed::Box;
+use core::mem;
+
+#[derive(Clone, Default, Debug)]
+pub struct OpenThreadRequest {
+    pub pid: u32,
+    pub tid: u32,
+    pub open_type: ObjectOpenType,
+}
+
+#[derive(Clone, Default, Debug)]
+pub struct CloseThreadRequest {
+    pub addr: u64,
+    pub open_type: ObjectOpenType,
+}
+
+#[derive(Clone,Default,Debug)]
+pub struct SuspendThreadRequest {
+    pub id: u32
+}
+
+impl VmcallRequest for SuspendThreadRequest{
+    type Response = ();
+
+    fn into_raw(self) -> *mut HypervisorRequest {
+        todo!()
+    }
+
+    fn from_raw(request: &HypervisorRequest) -> Self {
+        todo!()
+    }
+}
+
+impl VmcallRequest for CloseThreadRequest {
+    type Response = EmptyResponse;
+
+    fn into_raw(self) -> *mut HypervisorRequest {
+        let raw = Box::new(HypervisorRequest {
+            call: HypervisorCall::close_thread(),
+            arg1: self.addr.clone() as _,
+            arg2: self.open_type.clone().to_bits() as _,
+            ..Default::default()
+        });
+
+        mem::forget(self);
+
+        Box::into_raw(raw)
+    }
+
+    fn from_raw(request: &HypervisorRequest) -> Self {
+        Self {
+            addr: request.arg1 as _,
+            open_type: ObjectOpenType::from_bits(request.arg2 as _),
+        }
+    }
+}
+
+impl VmcallRequest for OpenThreadRequest {
+    type Response = OpenObjectResponse; // it works. that's all I can say
+
+    fn into_raw(self) -> *mut HypervisorRequest {
+        let raw = Box::new(HypervisorRequest {
+            call: match self.open_type.clone() {
+                ObjectOpenType::Handle => HypervisorCall::open_thread().with_is_async(true),
+                ObjectOpenType::Hypervisor => HypervisorCall::open_thread(),
+            },
+            arg1: self.pid as _,
+            arg2: self.tid as _,
+            arg3: self.open_type.clone().to_bits() as _,
+
+            ..Default::default()
+        });
+
+        mem::forget(self);
+
+        Box::into_raw(raw)
+    }
+
+    fn from_raw(request: &HypervisorRequest) -> Self {
+        Self {
+            pid: request.arg1 as _,
+            tid: request.arg2 as _,
+            open_type: ObjectOpenType::from_bits(request.arg3 as _),
+        }
+    }
+}
