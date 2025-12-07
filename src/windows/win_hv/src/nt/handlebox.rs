c2b744bfb785cdc36bdf164d085895a8be3d192d
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-07 19:00:02 +0300

    Keep handles counted, baby

---
+++ src/windows/win_hv/src/nt/handlebox.rs
@@ -0,0 +1,42 @@
+use core::mem;
+use wdk_sys::HANDLE;
+use wdk_sys::ntddk::ZwClose;
+
+pub struct HandleBox {
+    handle: HANDLE
+}
+
+impl HandleBox {
+    pub fn new(handle: HANDLE) -> HandleBox {
+        Self {
+            handle
+        }
+    }
+
+    ///
+    /// # Get Danger
+    ///
+    /// Gets the handle object. (Copy)
+    pub fn get_danger(&self) -> HANDLE{
+        self.handle
+    }
+
+    ///
+    /// # Get Forget
+    ///
+    /// Returns the handle, consumes the object, but does NOT close it.
+    pub fn get_forget(self) -> HANDLE {
+        let handle = self.handle;
+        mem::forget(self);
+
+        handle
+    }
+}
+
+impl Drop for HandleBox {
+    fn drop(&mut self) {
+        let _ = unsafe{
+            ZwClose(self.handle)
+        };
+    }
+}
