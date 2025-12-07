523d8e1853c0b763cdc8331b81ccc74ca94c773b
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-07 19:01:13 +0300

    Add suspend_resume_thread

--- src/windows/win_hv/src/services/thread_services.rs
+++ src/windows/win_hv/src/services/thread_services.rs
@@ -1,27 +1,81 @@
-use crate::plugins::commands::thread::OpenThreadAsyncCommand;
+use crate::nt::blanket::OpenHandle;
+use crate::plugins::commands::thread::*;
 use crate::plugins::{Plugin, PluginTable};
+use crate::win::{ZwResumeThread, ZwSuspendThread};
 use alloc::boxed::Box;
 use hv::hypervisor::host::Guest;
 use hxposed_core::hxposed::call::ServiceParameter;
 use hxposed_core::hxposed::error::NotFoundReason;
 use hxposed_core::hxposed::func::ServiceFunction;
-use hxposed_core::hxposed::requests::process::{CloseProcessRequest, ObjectOpenType};
+use hxposed_core::hxposed::requests::process::ObjectOpenType;
 use hxposed_core::hxposed::requests::thread::*;
 use hxposed_core::hxposed::responses::empty::{EmptyResponse, OpenObjectResponse};
+use hxposed_core::hxposed::responses::thread::SuspendThreadResponse;
 use hxposed_core::hxposed::responses::{HypervisorResponse, VmcallResponse};
 use hxposed_core::plugins::plugin_perms::PluginPermissions;
 use hxposed_core::services::async_service::UnsafeAsyncInfo;
-use wdk_sys::ntddk::{ObOpenObjectByPointer, PsLookupThreadByThreadId};
-use wdk_sys::_MODE::KernelMode;
-use wdk_sys::{PsThreadType, HANDLE, PETHREAD, STATUS_SUCCESS, THREAD_ALL_ACCESS};
+use wdk_sys::ntddk::PsLookupThreadByThreadId;
+use wdk_sys::{PETHREAD, STATUS_SUCCESS, ULONG};
 
-pub(crate) fn suspend_thread_async(
+pub(crate) fn suspend_resume_thread_sync(
+    request: &SuspendResumeThreadAsyncCommand,
+) -> HypervisorResponse {
+    let plugin = match PluginTable::lookup(request.uuid) {
+        Some(plugin) => plugin,
+        None => return HypervisorResponse::not_found_what(NotFoundReason::Plugin),
+    };
+
+    let thread = match plugin
+        .object_table
+        .get_open_thread(Some(request.command.id), None)
+    {
+        Some(thread) => thread,
+        None => return HypervisorResponse::not_found_what(NotFoundReason::Thread),
+    };
+
+    let handle = match thread.get_handle() {
+        Ok(handle) => handle,
+        Err(x) => return HypervisorResponse::nt_error(x as _),
+    };
+
+    let mut previous_count = ULONG::default();
+
+    match request.command.operation {
+        SuspendResumeThreadOperation::Suspend => {
+            match unsafe { ZwSuspendThread(handle.get_danger(), &mut previous_count) } {
+                STATUS_SUCCESS => SuspendThreadResponse { previous_count }.into_raw(),
+                err => HypervisorResponse::nt_error(err as u32),
+            }
+        }
+        SuspendResumeThreadOperation::Resume => {
+            match unsafe { ZwResumeThread(handle.get_danger(), &mut previous_count) } {
+                STATUS_SUCCESS => SuspendThreadResponse { previous_count }.into_raw(),
+                err => HypervisorResponse::nt_error(err as u32),
+            }
+        }
+        SuspendResumeThreadOperation::Freeze => {
+            HypervisorResponse::invalid_params(ServiceParameter::Function)
+        }
+    }
+}
+
+pub(crate) fn suspend_resume_thread_async(
     _guest: &mut dyn Guest,
-    request: SuspendThreadRequest,
+    request: SuspendResumeThreadRequest,
     plugin: &mut Plugin,
     async_info: UnsafeAsyncInfo,
 ) -> HypervisorResponse {
-    EmptyResponse::with_service(ServiceFunction::SuspendThread)
+    if !plugin.perm_check(PluginPermissions::THREAD_EXECUTIVE) {
+        return HypervisorResponse::not_allowed_perms(PluginPermissions::THREAD_EXECUTIVE);
+    }
+
+    plugin.queue_command(Box::new(SuspendResumeThreadAsyncCommand {
+        command: request,
+        uuid: plugin.uuid,
+        async_info,
+    }));
+
+    EmptyResponse::with_service(ServiceFunction::SuspendResumeThread)
 }
 
 ///
@@ -47,28 +101,17 @@ pub(crate) fn open_thread_sync(request: &OpenThreadAsyncCommand) -> HypervisorRe
     }
 
     match request.command.open_type {
-        ObjectOpenType::Handle => {
-            let mut handle = HANDLE::default();
-
-            match unsafe {
-                ObOpenObjectByPointer(
-                    thread as _,
-                    0,
-                    Default::default(),
-                    THREAD_ALL_ACCESS,
-                    *PsThreadType,
-                    KernelMode as _,
-                    &mut handle,
-                )
-            } {
-                STATUS_SUCCESS => OpenObjectResponse { addr: handle as _ }.into_raw(),
-                err => HypervisorResponse::nt_error(err as _),
-            }
+        ObjectOpenType::Handle => OpenObjectResponse {
+            addr: match thread.get_handle() {
+                Ok(handle) => handle.get_forget() as _,
+                Err(x) => return HypervisorResponse::nt_error(x as _),
+            },
         }
+        .into_raw(),
         ObjectOpenType::Hypervisor => {
             plugin.object_table.open_threads.push(thread);
             OpenObjectResponse { addr: thread as _ }.into_raw()
-        },
+        }
     }
 }
 
