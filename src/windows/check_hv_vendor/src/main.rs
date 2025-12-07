d9ffa6bd905c5a3847a601a921ae0bb31a4d2799
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-07 18:59:37 +0300

    Iterate threads and suspend them

--- src/windows/check_hv_vendor/src/main.rs
+++ src/windows/check_hv_vendor/src/main.rs
@@ -126,17 +126,39 @@ async fn async_main() {
         }
     };
 
-    println!("Threads: {:?}", threads);
+    for thread in &threads {
+        let mut thread = match HxThread::open(*thread) {
+            Ok(x) => x,
+            Err(e) => {
+                println!("Failed to get open thread: {:?}", e);
+                return;
+            }
+        };
 
-    let mut thread = match HxThread::open(threads[0]) {
-        Ok(x) => x,
-        Err(e) => {
-            println!("Failed to get open thread: {:?}", e);
-            return;
+        match thread.suspend().await {
+            Ok(c) => println!("Thread suspended. Previous count: {}", c),
+            Err(e) => println!("Failed to suspend thread: {:?}", e),
         }
-    };
+    }
 
-    thread.suspend().await;
+
+    println!("Successfully suspended threads! Press enter to resume them.");
+    stdin().read_line(&mut input).await.unwrap();
+
+    for thread in threads {
+        let mut thread = match HxThread::open(thread) {
+            Ok(x) => x,
+            Err(e) => {
+                println!("Failed to get open thread: {:?}", e);
+                return;
+            }
+        };
+
+        match thread.resume().await {
+            Ok(c) => println!("Thread resumed. Previous count: {}", c),
+            Err(e) => println!("Failed to resume thread: {:?}", e),
+        }
+    }
 
     /*println!("Address to read/write?: ");
     let mut input = String::new();
