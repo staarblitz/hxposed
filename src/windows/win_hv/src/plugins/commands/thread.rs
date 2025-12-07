a1bc481a1b154a5277d7d7c7daa24be268e085bd
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-07 19:00:36 +0300

    Implement async command for SuspendResumeThread

--- src/windows/win_hv/src/plugins/commands/thread.rs
+++ src/windows/win_hv/src/plugins/commands/thread.rs
@@ -1,6 +1,6 @@
 use core::any::Any;
 use hxposed_core::hxposed::func::ServiceFunction;
-use hxposed_core::hxposed::requests::thread::OpenThreadRequest;
+use hxposed_core::hxposed::requests::thread::*;
 use hxposed_core::hxposed::responses::HypervisorResponse;
 use hxposed_core::services::async_service::UnsafeAsyncInfo;
 use uuid::Uuid;
@@ -10,6 +10,28 @@ pub struct OpenThreadAsyncCommand {
     pub command: OpenThreadRequest,
     pub uuid: Uuid,
     pub async_info: UnsafeAsyncInfo,
+}
+
+pub struct SuspendResumeThreadAsyncCommand {
+    pub command: SuspendResumeThreadRequest,
+    pub uuid: Uuid,
+    pub async_info: UnsafeAsyncInfo,
+}
+
+impl AsyncCommand for SuspendResumeThreadAsyncCommand {
+    fn get_service_function(&self) -> ServiceFunction { ServiceFunction::SuspendResumeThread }
+
+    fn complete(&mut self, result: HypervisorResponse) {
+        write_and_set(
+            &result,
+            self.async_info.result_values as *mut _,
+            self.async_info.handle as _,
+        )
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
 }
 
 impl AsyncCommand for OpenThreadAsyncCommand {
