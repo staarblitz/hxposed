2eea09db16cdbdbf7fc39c88f4eaf847f3ec803b
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-07 15:57:10 +0300

    Add AsyncCommands for thread

---
+++ src/windows/win_hv/src/plugins/commands/thread.rs
@@ -0,0 +1,31 @@
+use core::any::Any;
+use hxposed_core::hxposed::func::ServiceFunction;
+use hxposed_core::hxposed::requests::thread::OpenThreadRequest;
+use hxposed_core::hxposed::responses::HypervisorResponse;
+use hxposed_core::services::async_service::UnsafeAsyncInfo;
+use uuid::Uuid;
+use crate::plugins::commands::{write_and_set, AsyncCommand};
+
+pub struct OpenThreadAsyncCommand {
+    pub command: OpenThreadRequest,
+    pub uuid: Uuid,
+    pub async_info: UnsafeAsyncInfo,
+}
+
+impl AsyncCommand for OpenThreadAsyncCommand {
+    fn get_service_function(&self) -> ServiceFunction {
+        ServiceFunction::OpenThread
+    }
+
+    fn complete(&mut self, result: HypervisorResponse) {
+        write_and_set(
+            &result,
+            self.async_info.result_values as *mut _,
+            self.async_info.handle as _,
+        )
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
