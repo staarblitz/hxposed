388b65766d553c54e648030e6e16ab5bad321042
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-07 18:59:55 +0300

    Blanket implementations for easier use

---
+++ src/windows/win_hv/src/nt/blanket.rs
@@ -0,0 +1,38 @@
+use wdk_sys::{PsProcessType, PsThreadType, ACCESS_MASK, HANDLE, NTSTATUS, PEPROCESS, PETHREAD, POBJECT_TYPE, PROCESS_ALL_ACCESS, PVOID, STATUS_SUCCESS, THREAD_ALL_ACCESS};
+use wdk_sys::_MODE::KernelMode;
+use wdk_sys::ntddk::ObOpenObjectByPointer;
+use crate::nt::handlebox::HandleBox;
+
+pub trait OpenHandle {
+    fn get_handle(&self) -> Result<HandleBox, NTSTATUS>;
+}
+
+impl OpenHandle for PEPROCESS {
+    fn get_handle(&self) -> Result<HandleBox, NTSTATUS> {
+        unsafe{get_handle_for_object(*self as _, *PsProcessType, PROCESS_ALL_ACCESS)}
+    }
+}
+
+impl OpenHandle for PETHREAD {
+    fn get_handle(&self) -> Result<HandleBox, NTSTATUS> {
+        unsafe{get_handle_for_object(*self as _, *PsThreadType, THREAD_ALL_ACCESS)}
+    }
+}
+
+fn get_handle_for_object(object: PVOID, obj_type: POBJECT_TYPE, rights: ACCESS_MASK) -> Result<HandleBox, NTSTATUS> {
+    let mut handle = HANDLE::default();
+    match unsafe {
+        ObOpenObjectByPointer(
+            object,
+            0,
+            Default::default(),
+            rights,
+            obj_type,
+            KernelMode as _,
+            &mut handle,
+        )
+    } {
+        STATUS_SUCCESS => Ok(HandleBox::new(handle)),
+        err => Err(err),
+    }
+}
