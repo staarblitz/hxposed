adb9ebd1ad596caf845fe8fc4dda048a9fdd543b
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-10 18:34:16 +0300

    Add GetThreadFieldResponse

--- src/hxposed_core/src/hxposed/responses/thread.rs
+++ src/hxposed_core/src/hxposed/responses/thread.rs
@@ -2,11 +2,50 @@ use crate::error::HypervisorError;
 use crate::hxposed::call::HypervisorResult;
 use crate::hxposed::func::ServiceFunction;
 use crate::hxposed::responses::{HypervisorResponse, VmcallResponse};
+use crate::hxposed::responses::process::GetProcessFieldResponse;
 
 #[derive(Clone, Debug, Default)]
 pub struct SuspendThreadResponse {
     pub previous_count: u32,
 }
+
+#[derive(Clone, Default, Debug)]
+#[repr(u16)]
+pub enum GetThreadFieldResponse {
+    #[default]
+    Unknown = 0,
+    ActiveImpersonationInfo(bool) = 1,
+    AdjustedClientToken(u64) = 2,
+}
+
+impl VmcallResponse for GetThreadFieldResponse {
+    fn from_raw(raw: HypervisorResponse) -> Result<Self, HypervisorError> {
+        if raw.result.is_error() {
+            return Err(HypervisorError::from_response(raw));
+        }
+
+        Ok(match raw.arg1 {
+            1 => Self::ActiveImpersonationInfo(raw.arg2 == 1),
+            _ => unreachable!("Developer forgot to implement this one."),
+        })
+    }
+
+    fn into_raw(self) -> HypervisorResponse {
+        let (arg1, arg2, arg3) = match self {
+            Self::ActiveImpersonationInfo(arg) => (1, arg as u64, 0),
+            Self::AdjustedClientToken(arg) => (2, arg, 0),
+            Self::Unknown => unreachable!(), // didn't use _ => on purpose, so I never forget implementing new ones
+        };
+
+        HypervisorResponse {
+            result: HypervisorResult::ok(ServiceFunction::GetThreadField),
+            arg1,
+            arg2,
+            arg3,
+        }
+    }
+}
+
 
 impl VmcallResponse for SuspendThreadResponse {
     fn from_raw(raw: HypervisorResponse) -> Result<Self, HypervisorError> {
