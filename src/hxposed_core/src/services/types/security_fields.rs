22cc72687b9556098ecc3f37f1fb07acb50eaa50
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-11 22:38:43 +0300

    Implement TokenType and ImpersonationLevel

--- src/hxposed_core/src/services/types/security_fields.rs
+++ src/hxposed_core/src/services/types/security_fields.rs
@@ -10,17 +10,47 @@ pub struct Luid {
 #[derive(Debug, Copy, Clone)]
 #[repr(C)]
 pub enum TokenType {
-    Primary,
-    Impersonation
+    Primary = 0,
+    Impersonation = 1
+}
+
+impl TokenType {
+    pub const fn into_bits(self) -> u8 {
+        self as _
+    }
+
+    pub const fn from_bits(bits: u8) -> Self {
+        match bits {
+            0 => Self::Primary,
+            1 => Self::Impersonation,
+            _ => unreachable!(),
+        }
+    }
 }
 
 #[derive(Debug, Copy, Clone)]
 #[repr(C)]
 pub enum ImpersonationLevel {
-    Anonymous,
-    Identification,
-    Impersonation,
-    Delegation
+    Anonymous = 0,
+    Identification = 1,
+    Impersonation = 2,
+    Delegation = 3
+}
+
+impl ImpersonationLevel {
+    pub const fn into_bits(self) -> u8 {
+        self as _
+    }
+
+    pub const fn from_bits(bits: u8) -> Self {
+        match bits {
+            0 => Self::Anonymous,
+            1 => Self::Identification,
+            2 => Self::Impersonation,
+            3 => Self::Delegation,
+            _ => unreachable!(),
+        }
+    }
 }
 
 pub struct TokenSource {
