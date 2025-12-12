735d9cd21aa5d342a89d439f5e86bad92db88055
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-12 14:02:08 +0300

    Implement GetTokenFieldAsyncCommand

--- src/windows/win_hv/src/plugins/commands/security.rs
+++ src/windows/win_hv/src/plugins/commands/security.rs
@@ -1,6 +1,6 @@
 use core::any::Any;
 use hxposed_core::hxposed::func::ServiceFunction;
-use hxposed_core::hxposed::requests::security::OpenTokenRequest;
+use hxposed_core::hxposed::requests::security::*;
 use hxposed_core::hxposed::responses::HypervisorResponse;
 use hxposed_core::services::async_service::UnsafeAsyncInfo;
 use uuid::Uuid;
@@ -10,6 +10,30 @@ pub struct OpenTokenAsyncCommand {
     pub command: OpenTokenRequest,
     pub uuid: Uuid,
     pub async_info: UnsafeAsyncInfo,
+}
+
+pub struct GetTokenFieldAsyncCommand {
+    pub command: GetTokenFieldRequest,
+    pub uuid: Uuid,
+    pub async_info: UnsafeAsyncInfo,
+}
+
+impl AsyncCommand for GetTokenFieldAsyncCommand {
+    fn get_service_function(&self) -> ServiceFunction {
+        ServiceFunction::GetTokenField
+    }
+
+    fn complete(&mut self, result: HypervisorResponse) {
+        write_and_set(
+            &result,
+            self.async_info.result_values as *mut _,
+            self.async_info.handle as _,
+        )
+    }
+
+    fn as_any(&self) -> &dyn Any {
+       self
+    }
 }
 
 impl AsyncCommand for OpenTokenAsyncCommand {
