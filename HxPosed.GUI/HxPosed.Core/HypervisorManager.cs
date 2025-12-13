49fcb17434dbafc3514e7c987d6bccbaf9da45ac
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-13 21:33:19 +0300

    Update bindings

--- HxPosed.GUI/HxPosed.Core/HypervisorManager.cs
+++ HxPosed.GUI/HxPosed.Core/HypervisorManager.cs
@@ -1,5 +1,7 @@
 ﻿using HxPosed.Core.Exceptions;
+using HxPosed.Core.Request;
 using HxPosed.Core.Response;
+using HxPosed.Plugins.Permissions;
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
@@ -12,17 +14,43 @@ namespace HxPosed.Core
     /// </summary>
     public class HypervisorManager
     {
-        [DllImport("libhxposed.dll", EntryPoint = "get_hx_state", CallingConvention = CallingConvention.Cdecl)]
-        private static extern HypervisorError GetHypervisorState(ref StatusResponse response);
+        [DllImport("libhxposed.dll")]
+        private static extern HypervisorError HxGetStatus(out HxStatus response);
+
+        [DllImport("libhxposed.dll")]
+        private static extern HypervisorError HxAuthenticate(ref AuthenticationRequest request, out PluginPermissions grantedPermissions);
+
 
         /// <summary>
-        /// Gets <see cref="StatusResponse"/> struct.
+        /// Authenticates the plugin.
+        /// </summary>
+        /// <param name="guid">Unique identifier of the plugin.</param>
+        /// <param name="permissions">Permission mask to utilize.</param>
+        /// <returns>Granted permissions. See <see cref="PluginPermissions"/></returns>
+        /// <exception cref="HypervisorException"></exception>
+        public static PluginPermissions Authenticate(Guid guid, PluginPermissions permissions)
+        {
+            var request = new AuthenticationRequest
+            {
+                Guid = guid,
+                Permissions = permissions
+            };
+
+            var error = HxAuthenticate(ref request, out var perms);
+
+            if (error.IsError())
+                throw new HypervisorException(error);
+
+            return perms;
+        }
+
+        /// <summary>
+        /// Gets <see cref="Status"/> struct.
         /// </summary>
         /// <exception cref="HypervisorException">Throws if hypervisor is not responding to CPUID traps.</exception>
-        public static StatusResponse GetHypervisorStatus()
+        public static HxStatus GetHypervisorStatus()
         {
-            var response = new StatusResponse();
-            var error = GetHypervisorState(ref response);
+            var error = HxGetStatus(out var response);
 
             if (error.IsError())
                 throw new HypervisorException(error);
