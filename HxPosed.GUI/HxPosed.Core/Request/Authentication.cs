71a807d79ee3cbb6196dfdc29b0977b5ecf0205d
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-13 21:33:41 +0300

    Update responses and requests

---
+++ HxPosed.GUI/HxPosed.Core/Request/Authentication.cs
@@ -0,0 +1,17 @@
+﻿using HxPosed.Plugins.Permissions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HxPosed.Core.Request
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct AuthenticationRequest
+    {
+        public Guid Guid;
+        public PluginPermissions Permissions;
+    }
+}
---
+++ HxPosed.GUI/HxPosed.Core/Response/Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace HxPosed.Core.Response
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct HxStatus
+    {
+        public HypervisorStatus Status;
+        public int Version;
+    }
+}
