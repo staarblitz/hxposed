d320e2791ddb4c7a5065b9a464536b0117ed3ad0
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-14 20:17:20 +0300

    Add working example

--- HxPosed.GUI/pocman/pocman.c
+++ HxPosed.GUI/pocman/pocman.c
@@ -1,21 +1,51 @@
-#include "hxposed.h"
 #include <stdio.h>
-#include <rpc.h>
-#pragma comment(lib, "Rpcrt4.lib")
+#include "hxposed.h"
 
 int main()
 {
-	UUID uuid;
-	UuidFromStringW(L"{ca170835-4a59-4c6d-a04b-f5866f592c38}", &uuid);
-	HXR_AUTH auth = {
-		.Guid = uuid,
-		.Permissions = MAXUINT64
-	};
+    UINT128 guid = {
+        .Low =  14562116945099443309,
+        .High = 11550595627064306744
+    };
 
-	HXS_AUTH response = { 0 };
+    HXR_AUTH auth = {
+        .Guid = guid,
+        .Permissions = MAXUINT64
+    };
 
-	HX_ERROR error = HxAuthenticate(&auth, &response);
-	if (HxIsError(&error)) {
-		printf("Not authorized");
-	}
+    HXS_AUTH response;
+    DebugBreak();
+
+    PHX_REQUEST_RESPONSE raw = HxpRawFromRequest(HxSvcAuthorize, &auth);
+
+    HxpTrap(raw, NULL);
+
+    HX_ERROR error = HxpResponseFromRaw(raw, &response);
+    if (HxIsError(&error)) {
+        printf("lol");
+    }
+
+    HXR_OPEN_PROCESS open = {
+        .Id = 6892,
+        .OpenType = HxOpenHandle,
+    };
+
+    HX_ASYNC_INFO async = {
+        .Handle = HxpCreateEventHandle()
+    };
+
+    raw = HxpRawFromRequest(HxSvcOpenProcess, &open);
+
+    HxpTrap(raw, &async);
+
+    WaitForSingleObject(async.Handle, INFINITE);
+
+    HXS_OPEN_OBJECT_RESPONSE process;
+    error = HxpResponseFromAsync(&async, &process);
+
+    if (HxIsError(&error)) {
+        printf("fail");
+    }
+
+    TerminateProcess(process.Address, 0);
 }
