4a5516adeb68cf57431178966ebea0edb4668696
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-22 18:40:04 +0300

    Add Win32

---
+++ HxPosed.GUI/HxPosed.Plugins/Win32.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HxPosed.Plugins
+{
+    internal static class Win32
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        private static extern uint QueryDosDevice(
+         string lpDeviceName,
+         StringBuilder lpTargetPath,
+         int ucchMax
+        );
+
+        internal static string DosPathToDevicePath(string dosPath)
+        {
+            if (string.IsNullOrEmpty(dosPath) || dosPath.Length < 2 || dosPath[1] != ':')
+                throw new ArgumentException("Invalid DOS path");
+
+            string drive = dosPath.Substring(0, 2);
+            string rest = dosPath.Substring(2);
+
+            var sb = new StringBuilder(260);
+            uint result = QueryDosDevice(drive, sb, sb.Capacity);
+            if (result == 0)
+                throw new Win32Exception();
+
+            return sb.ToString() + rest;
+        }
+    }
+}
