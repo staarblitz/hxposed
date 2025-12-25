b6b4efd7e5e87a39768748b809a6716bad736934
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-25 21:40:45 +0300

    Implement plugin load functionality in UI

--- HxPosed.GUI/HxPosed.GUI/Pages/Plugins.xaml.cs
+++ HxPosed.GUI/HxPosed.GUI/Pages/Plugins.xaml.cs
@@ -1,12 +1,19 @@
 ﻿using HxPosed.GUI.Models;
+using HxPosed.GUI.ViewModels;
 using HxPosed.Plugins;
+using HxPosed.Plugins.Config;
+using HxPosed.Plugins.Permissions;
+using Microsoft.Win32;
 using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using Wpf.Ui;
 using Wpf.Ui.Controls;
+using Wpf.Ui.Extensions;
 
 namespace HxPosed.GUI.Pages
 {
@@ -78,11 +85,116 @@ namespace HxPosed.GUI.Pages
                 return;
             }
 
-            Plugin.New(guid, nameTxt.Text, descTxt.Text, ver, urlTxt.Text, authTxt.Text, iconTxt.Text);
+            Plugin.New(guid, nameTxt.Text, descTxt.Text, ver, urlTxt.Text, authTxt.Text, iconTxt.Text, pathText.Text, PluginPermissions.None);
 
             var pageCtx = DataContext;
             DataContext = null;
             DataContext = pageCtx;
+        }
+
+        private CancellationTokenSource _cts = new();
+
+        private void CardAction_Click(object sender, RoutedEventArgs e)
+        {
+            // weird wpf com dialog shit
+            new Thread(async () =>
+            {
+                //var cfg = new PluginConfig
+                //{
+                //    Revision = 0,
+                //    Author = "Staarblitz",
+                //    Name = "HxTest",
+                //    Description = "Plugin to activate check_hv_vendor.exe",
+                //    Downloads = [],
+                //    Guid = Guid.Parse("ca170835-4a59-4c6d-a04b-f5866f592c38"),
+                //    Icon = "AddSubtract24",
+                //    Path = "Z:\\debug\\check_hv_vendor.exe",
+                //    Permissions = PluginPermissions.ProcessControl | PluginPermissions.CpuIO,
+                //    Url = "https://github.com/staarblitz/hxposed",
+                //    Version = 1
+                //};
+
+                //JsonSerializer.Serialize(cfg);
+
+                var dlg = new OpenFileDialog
+                {
+                    Filter = "HxPosed Plugin Config Files (.hpc)|*.hpc",
+                    Multiselect = false,
+                    Title = "Select a file",
+                    InitialDirectory = "C:\\"
+                };
+
+                if (dlg.ShowDialog() is not true)
+                    return;
+
+                ContentDialogService svc = null;
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    svc = new ContentDialogService();
+                    svc.SetDialogHost((Application.Current.MainWindow as MainWindow).presenter);
+                });
+
+                using var fs = File.OpenRead(dlg.FileName);
+                var config = await JsonSerializer.DeserializeAsync<PluginConfig>(fs);
+                if (config == null)
+                {
+                    await Application.Current.Dispatcher.InvokeAsync(async () =>
+                    {
+                        await svc.ShowSimpleDialogAsync(new SimpleContentDialogCreateOptions
+                        {
+                            Title = "Malformed Config",
+                            Content = "This config is malformed and cannnot be loaded",
+                            CloseButtonText = "Ok"
+                        });
+                    });
+                   
+                    return;
+                }
+
+                var fuckingLock = new SemaphoreSlim(1, 1);
+
+                if (config.Downloads.Count > 0)
+                {
+                    fuckingLock.Wait(); // we can enter it right away
+                    await Application.Current.Dispatcher.InvokeAsync(async () =>
+                    {
+                        switch(await svc.ShowSimpleDialogAsync(new SimpleContentDialogCreateOptions
+                        {
+                            Title = "HxPosed Plugins",
+                            Content = "This plugin contains downloads. Which can harm your computer. Continue?",
+                            CloseButtonText = "Cancel",
+                            PrimaryButtonText = "No",
+                            SecondaryButtonText = "Continue"
+                        }))
+                        {
+                            case ContentDialogResult.None:
+                            case ContentDialogResult.Primary:
+                                return;
+                        }
+
+                        fuckingLock.Release();
+                    });
+                }
+
+                await fuckingLock.WaitAsync();
+
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    var ctx = DataContext as PluginsViewModel;
+                    ctx.IsLoading = true;
+                });
+
+                await PluginManager.LoadFromConfig(config, _cts.Token).ContinueWith((x)=>
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        var ctx = DataContext;
+                        DataContext = null;
+                        DataContext = ctx;
+                    });
+                });
+            }).Start();
+
         }
     }
 }
