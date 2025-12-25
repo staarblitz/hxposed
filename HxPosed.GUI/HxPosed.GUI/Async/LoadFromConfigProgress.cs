8913570f2e13ea556868ba1bd87843450bcda94b
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-25 21:40:06 +0300

    .

---
+++ HxPosed.GUI/HxPosed.GUI/Async/LoadFromConfigProgress.cs
@@ -0,0 +1,16 @@
+﻿using HxPosed.Plugins.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HxPosed.GUI.Async
+{
+    internal class LoadFromConfigProgress
+    {
+        public string? CurrentFile { get; set; } = "";
+        public int? RemainingFiles { get; set; } = 0;
+        public CancellationToken Token { get; set; }
+    }
+}
