5a866ca7b1d69e91d5ff2d53c3158d58723ddefa
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-28 16:55:58 +0300

    Add scanner in case we need it

---
+++ src/windows/win_hv/src/utils/scanner.rs
@@ -0,0 +1,13 @@
+#![allow(dead_code)]
+
+use core::ptr::slice_from_raw_parts;
+
+pub struct Scanner;
+
+impl Scanner {
+    pub fn pattern_scan(base: *const u8, len: usize, pattern: &[u8]) -> Option<*const u8> {
+        let slice = unsafe {slice_from_raw_parts(base, len).as_ref().unwrap()};
+
+        slice.windows(pattern.len()).find(|window| *window == pattern).map(|window| window.as_ptr())
+    }
+}
