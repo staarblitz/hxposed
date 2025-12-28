62d106da27d6a62064dea8b7efb76b301f4bd0a2
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2025-12-28 16:54:33 +0300

    USe KeIpiGenericCall

--- src/windows/win_hv/src/ops.rs
+++ src/windows/win_hv/src/ops.rs
@@ -1,42 +1,14 @@
-//! This module implements Windows kernel driver-based implementation of
-//! [`hv::PlatformOps`].
-
-use core::convert::TryFrom;
+use core::mem;
 use hv::platform_ops::PlatformOps;
-use wdk_sys::{
-    ALL_PROCESSOR_GROUPS, GROUP_AFFINITY, NT_SUCCESS, PAGED_CODE, PROCESSOR_NUMBER,
-    ntddk::{
-        KeGetProcessorNumberFromIndex, KeQueryActiveProcessorCountEx,
-        KeRevertToUserGroupAffinityThread, KeSetSystemGroupAffinityThread, MmGetPhysicalAddress,
-    },
-};
+use wdk_sys::ntddk::KeIpiGenericCall;
+use wdk_sys::{ULONG_PTR, ntddk::MmGetPhysicalAddress};
 
 pub(crate) struct WindowsOps;
 
 impl PlatformOps for WindowsOps {
     fn run_on_all_processors(&self, callback: fn()) {
-        fn processor_count() -> u32 {
-            unsafe { KeQueryActiveProcessorCountEx(u16::try_from(ALL_PROCESSOR_GROUPS).unwrap()) }
-        }
-
-        PAGED_CODE!();
-
-        for index in 0..processor_count() {
-            let mut processor_number = PROCESSOR_NUMBER::default();
-            let status = unsafe { KeGetProcessorNumberFromIndex(index, &raw mut processor_number) };
-            assert!(NT_SUCCESS(status));
-
-            let mut old_affinity = GROUP_AFFINITY::default();
-            let mut affinity = GROUP_AFFINITY {
-                Group: processor_number.Group,
-                Mask: 1 << processor_number.Number,
-                Reserved: [0, 0, 0],
-            };
-            unsafe { KeSetSystemGroupAffinityThread(&raw mut affinity, &raw mut old_affinity) };
-
-            callback();
-
-            unsafe { KeRevertToUserGroupAffinityThread(&raw mut old_affinity) };
+        unsafe {
+            KeIpiGenericCall(Some(ipi_broadcast_worker), callback as _);
         }
     }
 
@@ -46,4 +18,12 @@ impl PlatformOps for WindowsOps {
             MmGetPhysicalAddress(va.cast_mut()).QuadPart as u64
         }
     }
+}
+
+#[allow(unsafe_op_in_unsafe_fn)]
+unsafe extern "C" fn ipi_broadcast_worker(argument: ULONG_PTR) -> ULONG_PTR {
+    let callback: fn() = mem::transmute(argument);
+    callback();
+
+    ULONG_PTR::default()
 }
