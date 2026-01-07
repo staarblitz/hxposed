e8c4d703daeeefc9e83c511654f774bb8ffa1e8a
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2026-01-07 17:44:13 +0300

    Add serial logging

--- src/windows/win_hv/src/utils/logger.rs
+++ src/windows/win_hv/src/utils/logger.rs
@@ -1,7 +1,33 @@
+use alloc::string::ToString;
+use com_logger::Serial;
+use core::cell::OnceCell;
+use core::fmt::Write;
 use log::{Metadata, Record};
-use wdk::println;
+use spin::Mutex;
+use wdk::{print};
 
-pub struct NtLogger;
+pub struct NtLogger {
+    serial: Mutex<OnceCell<Serial>>,
+    pub is_init: bool,
+}
+
+unsafe impl Send for NtLogger {}
+unsafe impl Sync for NtLogger {}
+
+impl NtLogger {
+    pub const fn default() -> Self {
+        Self {
+            serial: Mutex::new(OnceCell::new()),
+            is_init: false,
+        }
+    }
+
+    pub fn init(&mut self) {
+        let lock = self.serial.lock();
+        let _ = lock.set(Serial::new(0x3f8));
+        self.is_init = true;
+    }
+}
 
 impl log::Log for NtLogger {
     fn enabled(&self, _metadata: &Metadata) -> bool {
@@ -9,13 +35,22 @@ impl log::Log for NtLogger {
     }
 
     fn log(&self, record: &Record) {
-        println!(
-            "[HXPOSED] [{}] - {} ({}@{})",
+        let args = format_args!(
+            "{:>8}: {} ({}, {}:{})\n",
             record.level(),
             record.args(),
-            record.file().unwrap_or("No file"),
-            record.line().unwrap_or(0)
-        );
+            record.target(),
+            record.file().unwrap_or("<unknown>"),
+            record.line().unwrap_or(0),
+        )
+        .to_string();
+
+        {
+            let mut lock = self.serial.lock();
+            let _ = lock.get_mut().unwrap().write_str(args.as_str());
+        }
+
+        print!("{}", args);
     }
 
     fn flush(&self) {}
