a4634b6b89dcd454f2ab38c0ab5817f8eb330e7c
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2026-01-07 17:36:09 +0300

    Cooler error handling

--- HxPosed.GUI/HxPosed.GUI/App.xaml.cs
+++ HxPosed.GUI/HxPosed.GUI/App.xaml.cs
@@ -2,7 +2,11 @@
 using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Controls;
+using Wpf.Ui;
 using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
+using Wpf.Ui.Extensions;
 
 namespace HxPosed.GUI
 {
@@ -11,21 +15,48 @@ namespace HxPosed.GUI
     /// </summary>
     public partial class App : Application
     {
+        public static ContentDialogService ContentDialogService { get; } = new ContentDialogService();
+        
+
+        public static NavigationView MainWindowNavigationView
+        {
+            get
+            {
+                // not null. come on C#.
+                return (Current.MainWindow as MainWindow).navView;
+            }
+        }
+
         public App()
         {
             PluginManager.HealthCheck();
             DispatcherUnhandledException += (sender, exception) =>
             {
-                MessageBox.Show(exception.Exception.ToString());
+                ContentDialogService.ShowSimpleDialogAsync(new SimpleContentDialogCreateOptions
+                {
+                    Title = "Error",
+                    Content = $"An error occured: {exception.Exception.Message}\n{exception.Exception}",
+                    CloseButtonText = "OK"
+                });
                 exception.Handled = true;
             };
             AppDomain.CurrentDomain.UnhandledException += (sender, exception) =>
             {
-                MessageBox.Show(((Exception)exception.ExceptionObject).ToString());
+                ContentDialogService.ShowSimpleDialogAsync(new SimpleContentDialogCreateOptions
+                {
+                    Title = "Error",
+                    Content = $"An error occured: {(exception.ExceptionObject as Exception).Message}\n{exception.ExceptionObject as Exception}",
+                    CloseButtonText = "OK"
+                });
             };
             TaskScheduler.UnobservedTaskException += (sender, exception) =>
             {
-                MessageBox.Show(exception.Exception.ToString());
+                ContentDialogService.ShowSimpleDialogAsync(new SimpleContentDialogCreateOptions
+                {
+                    Title = "Error",
+                    Content = $"An error occured: {exception.Exception.Message}\n{exception.Exception}",
+                    CloseButtonText = "OK"
+                });
                 exception.SetObserved();
             };
         }
