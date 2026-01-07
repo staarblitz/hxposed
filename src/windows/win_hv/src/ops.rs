d61a4ddf93fd0e94c81abb2b3b1a14f92777af28
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2026-01-07 17:42:24 +0300

    Return back to old

--- src/windows/win_hv/src/ops.rs
+++ src/windows/win_hv/src/ops.rs
@@ -1,14 +1,37 @@
-use core::mem;
 use hv::platform_ops::PlatformOps;
-use wdk_sys::ntddk::KeIpiGenericCall;
-use wdk_sys::{ULONG_PTR, ntddk::MmGetPhysicalAddress};
+use wdk_sys::ntddk::{
+    KeGetProcessorNumberFromIndex, KeQueryActiveProcessorCountEx,
+    KeRevertToUserGroupAffinityThread, KeSetSystemGroupAffinityThread,
+};
+use wdk_sys::{
+    ntddk::MmGetPhysicalAddress, ALL_PROCESSOR_GROUPS, GROUP_AFFINITY, NT_SUCCESS,
+    PROCESSOR_NUMBER,
+};
 
 pub(crate) struct WindowsOps;
 
 impl PlatformOps for WindowsOps {
     fn run_on_all_processors(&self, callback: fn()) {
-        unsafe {
-            KeIpiGenericCall(Some(ipi_broadcast_worker), callback as _);
+        fn processor_count() -> u32 {
+            unsafe { KeQueryActiveProcessorCountEx(u16::try_from(ALL_PROCESSOR_GROUPS).unwrap()) }
+        }
+
+        for index in 0..processor_count() {
+            let mut processor_number = PROCESSOR_NUMBER::default();
+            let status = unsafe { KeGetProcessorNumberFromIndex(index, &raw mut processor_number) };
+            assert!(NT_SUCCESS(status));
+
+            let mut old_affinity = GROUP_AFFINITY::default();
+            let mut affinity = GROUP_AFFINITY {
+                Group: processor_number.Group,
+                Mask: 1 << processor_number.Number,
+                Reserved: [0, 0, 0],
+            };
+            unsafe { KeSetSystemGroupAffinityThread(&raw mut affinity, &raw mut old_affinity) };
+
+            callback();
+
+            unsafe { KeRevertToUserGroupAffinityThread(&raw mut old_affinity) };
         }
     }
 
@@ -18,12 +41,4 @@ impl PlatformOps for WindowsOps {
             MmGetPhysicalAddress(va.cast_mut()).QuadPart as u64
         }
     }
-}
-
-#[allow(unsafe_op_in_unsafe_fn)]
-unsafe extern "C" fn ipi_broadcast_worker(argument: ULONG_PTR) -> ULONG_PTR {
-    let callback: fn() = mem::transmute(argument);
-    callback();
-
-    ULONG_PTR::default()
 }
