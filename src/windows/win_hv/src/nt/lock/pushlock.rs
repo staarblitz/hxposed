f70efeb68cfe370656cf3d9ee803866f9bfbfd3c
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2026-01-10 22:38:27 +0300

    Abstraction over EX_PUSH_LOCK

---
+++ src/windows/win_hv/src/nt/lock/pushlock.rs
@@ -0,0 +1,57 @@
+use wdk_sys::ntddk::{
+    ExAcquirePushLockExclusiveEx, ExAcquirePushLockSharedEx, ExReleasePushLockExclusiveEx,
+    ExReleasePushLockSharedEx,
+};
+
+pub struct PushLock {
+    lock: *mut u64,
+}
+
+unsafe impl Sync for PushLock {}
+unsafe impl Send for PushLock {}
+
+impl PushLock {
+    pub unsafe fn from_ptr(ptr: *mut u64) -> Self {
+        Self { lock: ptr }
+    }
+
+    pub fn acquire_exclusive(&mut self) -> PushLockGuard {
+        PushLockGuard::new(self.lock, LockType::Exclusive)
+    }
+
+    pub fn acquire_shared(&self) -> PushLockGuard {
+        PushLockGuard::new(self.lock, LockType::Shared)
+    }
+}
+
+enum LockType {
+    Exclusive,
+    Shared,
+}
+
+pub struct PushLockGuard {
+    lock: *mut u64,
+    lock_type: LockType,
+}
+
+impl Drop for PushLockGuard {
+    fn drop(&mut self) {
+        match self.lock_type {
+            LockType::Exclusive => unsafe { ExReleasePushLockExclusiveEx(self.lock, 0) },
+            LockType::Shared => unsafe { ExReleasePushLockSharedEx(self.lock, 0) },
+        }
+    }
+}
+
+impl PushLockGuard {
+    pub(crate) fn new(lock: *mut u64, lock_type: LockType) -> Self {
+        let me = Self { lock, lock_type };
+
+        match me.lock_type {
+            LockType::Exclusive => unsafe { ExAcquirePushLockExclusiveEx(lock, 0) },
+            LockType::Shared => unsafe { ExAcquirePushLockSharedEx(lock, 0) },
+        }
+
+        me
+    }
+}
