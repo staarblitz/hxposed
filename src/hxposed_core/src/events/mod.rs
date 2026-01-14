6f7618ed4a3c66a9c986f4be33067bea7ad805e7
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2026-01-14 12:42:28 +0300

    Move unsafe async info

--- src/hxposed_core/src/events/mod.rs
+++ src/hxposed_core/src/events/mod.rs
@@ -1,5 +1,23 @@
 use alloc::boxed::Box;
 use spin::Mutex;
+use crate::hxposed::ProcessObject;
+
+#[derive(Default, Debug)]
+pub struct UnsafeAsyncInfo {
+    pub handle: u64,
+    pub result_values: *mut [u64; 4],
+    pub process: ProcessObject
+}
+
+unsafe impl Send for UnsafeAsyncInfo {}
+unsafe impl Sync for UnsafeAsyncInfo {}
+
+impl UnsafeAsyncInfo {
+    pub fn is_present(&self) -> bool {
+        self.handle != 0
+    }
+}
+
 
 #[cfg(feature = "usermode")]
 pub mod async_service;
