90ccc00b5bdcec0bbe9d3a02d198ba79b692ceba
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2026-01-14 12:45:32 +0300

    Add caller verification

--- src/windows/win_hv/src/nt/guard/hxguard.rs
+++ src/windows/win_hv/src/nt/guard/hxguard.rs
@@ -1,44 +1,160 @@
 use crate::nt::process::NtProcess;
+use crate::nt::registry::NtKey;
 use crate::utils::alloc::PoolAllocSized;
 use crate::win::utf_to_unicode::Utf8ToUnicodeString;
-use crate::win::utils::{_RtlDuplicateUnicodeString, RtlBufferContainsBuffer};
-use wdk_sys::_MODE::KernelMode;
+use crate::win::utils::{RtlBufferContainsBuffer, _RtlDuplicateUnicodeString};
+use core::arch::x86_64::{
+    __m128i, _mm_cmpeq_epi64, _mm_loadu_si128, _mm_movemask_pd, _mm_set1_epi64x,
+};
+use wdk::dbg_break;
 use wdk_sys::ntddk::*;
+use wdk_sys::_MODE::KernelMode;
 use wdk_sys::*;
+
+pub static mut VALID_CALLERS: [u64; 256] = [0; 256];
+//pub static VALID_CALLER_COUNT: AtomicU32 = AtomicU32::new(0);
+
+pub enum RegistryProtection {
+    Initialized(u64),
+    Enabled,
+    Disabled,
+}
 
 /// Protects HxPosed from unauthorized access
 pub struct HxGuard {
-    pub registry_guard_enabled: bool,
-    registry_callback_cookie: u64,
+    pub registry_protection: RegistryProtection,
+    pub caller_verification: bool,
 }
 
 impl HxGuard {
-    pub const fn new(registry_guard_enabled: bool) -> Self {
+    pub const fn new() -> Self {
         Self {
-            registry_guard_enabled,
-            registry_callback_cookie: 0,
+            registry_protection: RegistryProtection::Disabled,
+            caller_verification: false,
         }
     }
 
+    // not my code
+    #[allow(static_mut_refs)]
+    pub fn is_valid_caller(hash: u64) -> bool {
+        unsafe {
+            let mut i = 0;
+            let len = VALID_CALLERS.len();
+
+            let needle_vec = _mm_set1_epi64x(hash as i64);
+
+            while i + 2 <= len {
+                let ptr = VALID_CALLERS.as_ptr().add(i) as *const __m128i;
+                let chunk = _mm_loadu_si128(ptr);
+
+                let cmp = _mm_cmpeq_epi64(chunk, needle_vec);
+                let mask = _mm_movemask_pd(core::mem::transmute(cmp));
+
+                if mask != 0 {
+                    return true;
+                }
+
+                i += 2;
+            }
+
+            false
+        }
+    }
+
+    #[allow(static_mut_refs)]
     pub fn init(&mut self) {
         log::info!("Initializing HxGuard");
-        if self.registry_guard_enabled {
-            log::info!("Registry Guard is enabled.");
-            match unsafe {
-                CmRegisterCallback(
-                    Some(Self::registry_callback),
-                    PVOID::default(),
-                    &mut self.registry_callback_cookie as *mut _ as _,
-                )
-            } {
-                STATUS_SUCCESS => {
-                    log::info!("Registry Callbacks are being watched by HxGuard.")
-                }
-                err => {
-                    panic!("Error registering registry callbacks: {:?}", err);
-                }
+        let key = match NtKey::open("\\Registry\\Machine\\SOFTWARE\\HxPosed\\HxGuard") {
+            Ok(x) => x,
+            Err(err) => {
+                panic!("Failed to open HxPosed key: {:x}", err)
             }
+        };
+
+        self.caller_verification = match key.get_value::<BOOLEAN>("CallerVerification") {
+            Ok(x) => *x == 1,
+            Err(err) => {
+                log::warn!(
+                    "Failed to read CallerVerification. Reverting to default (TRUE): {:x}",
+                    err
+                );
+                true
+            }
+        };
+
+        self.registry_protection = match key.get_value::<BOOLEAN>("RegistryProtection") {
+            Ok(x) => match x {
+                1 => RegistryProtection::Enabled,
+                0 => RegistryProtection::Disabled,
+                _ => unreachable!(),
+            },
+            Err(err) => {
+                log::warn!(
+                    "Failed to read RegistryProtection. Reverting to default (TRUE): {:x}",
+                    err
+                );
+                RegistryProtection::Enabled
+            }
+        };
+
+        if self.caller_verification {
+            log::info!("Caller verification is enabled.");
+
+            let key = match NtKey::open(
+                "\\Registry\\Machine\\SOFTWARE\\HxPosed\\HxGuard\\CallerVerification",
+            ) {
+                Ok(x) => x,
+                Err(err) => {
+                    panic!("Failed to open CallerVerification key: {:x}", err)
+                }
+            };
+
+            let mut default: [u64; 256] = [0; 256];
+            let values = match key.get_value::<[u64; 256]>("VerifiedCallers") {
+                Ok(x) => x,
+                Err(err) => {
+                    log::error!(
+                        "Failed to read VerifiedCallers key! No caller is allowed. {:x}",
+                        err
+                    );
+                    &mut default
+                }
+            };
+
+            // SAFETY: no one touches VALID_CALLERS yet.
+            unsafe {
+                core::ptr::copy_nonoverlapping::<u64>(
+                    values.as_ptr(),
+                    VALID_CALLERS.as_mut_ptr(),
+                    values.len(),
+                );
+            }
+
+            log::info!("Caller verification is active");
         }
+        match self.registry_protection {
+            RegistryProtection::Enabled => {
+                let mut cookie = UINT64::default();
+                log::info!("Registry protection is enabled.");
+                match unsafe {
+                    CmRegisterCallback(
+                        Some(Self::registry_callback),
+                        PVOID::default(),
+                        &mut cookie as *mut _ as _,
+                    )
+                } {
+                    STATUS_SUCCESS => {
+                        log::info!("Registry callbacks are being watched by HxGuard.")
+                    }
+                    err => {
+                        panic!("Error registering registry callbacks: {:?}", err);
+                    }
+                }
+                self.registry_protection = RegistryProtection::Initialized(cookie);
+            }
+            RegistryProtection::Disabled => {}
+            _ => unreachable!(),
+        };
     }
 
     ///
@@ -90,7 +206,7 @@ impl HxGuard {
                 match unsafe {
                     ObQueryNameString(op_info.RootObject, alloc.as_mut(), ret_len, &mut ret_len)
                 } {
-                    STATUS_SUCCESS => {},
+                    STATUS_SUCCESS => {}
                     err => {
                         log::warn!("Failed to query object name: {:x}", err);
                         return STATUS_SUCCESS;
@@ -104,7 +220,8 @@ impl HxGuard {
                 let _ = unsafe {
                     RtlAppendUnicodeStringToString(dup.as_mut(), "\\".to_unicode_string().as_mut())
                 };
-                let _ = unsafe { RtlAppendUnicodeStringToString(dup.as_mut(), op_info.RemainingName) };
+                let _ =
+                    unsafe { RtlAppendUnicodeStringToString(dup.as_mut(), op_info.RemainingName) };
 
                 let path = "SOFTWARE\\HxPosed".to_unicode_string();
 
@@ -148,5 +265,4 @@ impl HxGuard {
         }
         STATUS_SUCCESS
     }
-
 }
