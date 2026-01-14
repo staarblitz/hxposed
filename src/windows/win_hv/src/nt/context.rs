3501719cb32419dd93efaeaa4262b4aa8c42a452
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2026-01-14 12:45:22 +0300

    cargo fix

--- src/windows/win_hv/src/nt/context.rs
+++ src/windows/win_hv/src/nt/context.rs
@@ -1,16 +1,18 @@
-use wdk_sys::{KAPC_STATE, PEPROCESS};
 use wdk_sys::ntddk::{KeStackAttachProcess, KeUnstackDetachProcess};
+use wdk_sys::{KAPC_STATE, PEPROCESS};
 
-pub struct ApcProcessContext{
-    apc_state: KAPC_STATE
+pub struct ApcProcessContext {
+    apc_state: KAPC_STATE,
 }
 
-impl ApcProcessContext{
-    pub fn begin(process: PEPROCESS) -> Self{
+impl ApcProcessContext {
+    pub fn begin(process: PEPROCESS) -> Self {
         log::trace!("Begin process APC context {:x}", process as u64);
 
-        let mut result = Self { apc_state: Default::default()};
-        unsafe{
+        let mut result = Self {
+            apc_state: Default::default(),
+        };
+        unsafe {
             KeStackAttachProcess(process, &mut result.apc_state);
         }
 
@@ -18,10 +20,13 @@ impl ApcProcessContext{
     }
 }
 
-impl Drop for ApcProcessContext{
-    fn drop(&mut self){
-        log::trace!("End process APC context {:x}", self.apc_state.Process as u64);
-        unsafe{
+impl Drop for ApcProcessContext {
+    fn drop(&mut self) {
+        log::trace!(
+            "End process APC context {:x}",
+            self.apc_state.Process as u64
+        );
+        unsafe {
             KeUnstackDetachProcess(&mut self.apc_state);
         }
     }
