01085750e449fda3b33df18683842ba7d57ae3d5
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2026-01-16 11:42:07 +0300

    BREAKING: Async system revisited
    Added cancellation support.
    Fixed ownership semantics.
    Added timeout.

--- src/hxposed_core/src/events/async_service.rs
+++ src/hxposed_core/src/events/async_service.rs
@@ -6,85 +6,27 @@ use crate::intern::instructions::vmcall;
 #[cfg(feature = "usermode")]
 use crate::intern::win::{CloseHandle, CreateEventA, CreateThread, SetEvent, WaitForSingleObject};
 use alloc::boxed::Box;
-use atomic_enum::atomic_enum;
+use alloc::sync::Arc;
 use core::marker::PhantomData;
 use core::pin::Pin;
 use core::ptr::null_mut;
-use core::sync::atomic::{AtomicPtr, Ordering};
-use core::task::{Context, Poll, Waker};
+use core::sync::atomic::{Ordering};
+use core::task::{Context, Poll};
 use spin::Mutex;
-use crate::events::AsyncInfo;
-
-#[derive(Debug, Default)]
-pub struct WakerCell {
-    ptr: AtomicPtr<Waker>,
-}
-
-impl WakerCell {
-    pub const fn new() -> Self {
-        Self {
-            ptr: AtomicPtr::new(null_mut()),
-        }
-    }
-
-    pub fn register(&self, w: &Waker) {
-        let boxed = Box::into_raw(Box::new(w.clone()));
-        let prev = self.ptr.swap(boxed, Ordering::AcqRel);
-        if !prev.is_null() {
-            // SAFETY: prev was a Box<Waker> allocated above
-            unsafe { drop(Box::from_raw(prev)) };
-        }
-    }
-
-    pub fn take(&self) -> Option<Waker> {
-        let p = self.ptr.swap(null_mut(), Ordering::AcqRel);
-        if p.is_null() {
-            None
-        } else {
-            // safety: p is Box<Waker>
-            let boxed = unsafe { Box::from_raw(p) };
-            Some(*boxed)
-        }
-    }
-
-    pub fn wake_and_clear(&self) {
-        if let Some(w) = self.take() {
-            w.wake();
-        }
-    }
-}
-
-impl Drop for WakerCell {
-    fn drop(&mut self) {
-        let p = self.ptr.load(Ordering::Acquire);
-        if !p.is_null() {
-            unsafe { drop(Box::from_raw(p)) };
-        }
-    }
-}
-
-#[atomic_enum]
-pub enum PromiseState {
-    None,
-    Waiting,
-    Completed,
-}
+use crate::events::{AsyncInfo, AtomicPromiseState, PromiseState, WakerCell};
 
 #[derive(Debug)]
 pub struct AsyncPromise<RQ, RS> {
-    pub async_info: AsyncInfo,
-    pub state: AtomicPromiseState,
-    waker: WakerCell,
+    pub async_info: Arc<AsyncInfo>,
     phantom: PhantomData<RS>,
     request: Option<RQ>,
     raw_request: Option<HypervisorRequest>,
-    cancellation_requested: bool
 }
 impl<T, X> Unpin for AsyncPromise<T, X> {}
 
 impl<RQ, RS> Drop for AsyncPromise<RQ, RS> {
     fn drop(&mut self) {
-        self.cancellation_requested = true;
+        self.async_info.state.store(PromiseState::CancelPending, Ordering::Relaxed);
         // wake up the waiting thread
         unsafe{
             SetEvent(self.async_info.handle);
@@ -100,34 +42,45 @@ where
     type Output = Result<RS, HypervisorError>;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let me = unsafe { self.get_unchecked_mut() };
-
-        match me.state.load(Ordering::Acquire) {
+        let me = Pin::get_mut(self);
+        
+        match me.async_info.state.load(Ordering::Relaxed) {
             PromiseState::None => {
-                me.waker.register(cx.waker());
-                let pinned_me = unsafe{Pin::new_unchecked(me).get_unchecked_mut()};
+                me.async_info.state.store(PromiseState::Waiting, Ordering::Relaxed);
+                me.async_info.waker.register(cx.waker());
+
+                let shared_ptr = Arc::into_raw(me.async_info.clone());
+
                 match unsafe {
                     CreateThread(
                         null_mut(),
                         0,
                         Self::hv_wait_worker,
-                        pinned_me as *mut _ as _,
+                        shared_ptr as _,
                         0,
                         null_mut(),
                     )
                 } {
-                    0 => panic!("CreateThread returned an error"),
+                    0 => {
+                        // reclaim mem
+                        /*unsafe{
+                            drop(Arc::from_raw(shared_ptr));
+                        }*/
+                        panic!("CreateThread returned an error")
+                    }
                     handle => unsafe {
                         CloseHandle(handle)
                     },
                 };
                 Poll::Pending
             }
-            PromiseState::Waiting => Poll::Pending,
+            PromiseState::Waiting => {
+                me.async_info.waker.register(cx.waker());
+                Poll::Pending
+            },
+            PromiseState::CancelPending => Poll::Ready(Err(HypervisorError::async_cancel())),
+            PromiseState::TimedOut => Poll::Ready(Err(HypervisorError::async_time_out())),
             PromiseState::Completed => {
-                // unsafe {
-                //     asm!("int 0x3")
-                // }
                 Poll::Ready(me.get_result())
             }
         }
@@ -141,23 +94,24 @@ where
 {
     #[allow(unsafe_op_in_unsafe_fn)]
     unsafe extern "C" fn hv_wait_worker(param: *mut u64) -> u32 {
-        let me = &mut *(param as *mut AsyncPromise<RQ, RS>);
-        match WaitForSingleObject(me.async_info.handle, 2000) {
+        let info = Arc::from_raw(param as *const AsyncInfo);
+
+        let complete = |state: PromiseState| {
+            info.state.store(state, Ordering::Relaxed);
+            info.waker.wake_and_clear();
+        };
+
+        match WaitForSingleObject(info.handle, 2000) {
             0x00000102 /* WAIT_TIMEOUT*/ => {
-                panic!("Hypervisor failed to complete async task. This indicates a bug on kernel side.")
+                complete(PromiseState::TimedOut)
             }
-            _ => {}
+            0 => {}
+            err => {
+                panic!("WaitForSingleObject failed: {:x}", err);
+            }
         }
 
-        // check if cancellation requested, if so, do nothing.
-        if me.cancellation_requested {
-            return 0;
-        }
-
-        me.state.store(PromiseState::Completed, Ordering::Release);
-        if let Some(waker) = me.waker.take() {
-            waker.wake();
-        }
+        complete(PromiseState::Completed);
 
         0
     }
@@ -190,7 +144,7 @@ where
 
         let mut raw_request = request.into_raw();
 
-        let response = vmcall(&mut raw_request, Some(&mut self.async_info));
+        let response = vmcall(&mut raw_request, Some(self.async_info.clone()));
 
         // save it for later
         self.raw_request = Some(raw_request);
@@ -214,14 +168,13 @@ where
         Box::pin(AsyncPromise::<RQ, RS> {
             request: Some(request),
             raw_request: None,
-            state: AtomicPromiseState::new(PromiseState::None),
-            waker: WakerCell::new(),
             phantom: PhantomData,
-            cancellation_requested: false,
-            async_info: AsyncInfo {
+            async_info: Arc::new(AsyncInfo {
                 handle: unsafe { CreateEventA(null_mut(), 0, 0, null_mut()) },
                 result_values: Mutex::new(Box::new([0; 4])),
-            },
+                waker: WakerCell::new(),
+                state: AtomicPromiseState::new(PromiseState::None),
+            }),
         })
     }
 }
--- src/hxposed_core/src/events/mod.rs
+++ src/hxposed_core/src/events/mod.rs
@@ -1,6 +1,9 @@
+use core::{ptr::null_mut, sync::atomic::{AtomicBool, AtomicPtr, Ordering}, task::Waker};
+
 use alloc::boxed::Box;
 use spin::Mutex;
 use crate::hxposed::ProcessObject;
+use atomic_enum::atomic_enum;
 
 #[derive(Default, Debug)]
 pub struct UnsafeAsyncInfo {
@@ -22,8 +25,68 @@ impl UnsafeAsyncInfo {
 #[cfg(feature = "usermode")]
 pub mod async_service;
 
-#[derive(Default, Debug)]
+#[derive(Debug, Default)]
+pub struct WakerCell {
+    ptr: AtomicPtr<Waker>,
+}
+
+
+impl WakerCell {
+    pub const fn new() -> Self {
+        Self {
+            ptr: AtomicPtr::new(null_mut()),
+        }
+    }
+
+    pub fn register(&self, w: &Waker) {
+        let boxed = Box::into_raw(Box::new(w.clone()));
+        let prev = self.ptr.swap(boxed, Ordering::AcqRel);
+        if !prev.is_null() {
+            // SAFETY: prev was a Box<Waker> allocated above
+            unsafe { drop(Box::from_raw(prev)) };
+        }
+    }
+
+    pub fn take(&self) -> Option<Waker> {
+        let p = self.ptr.swap(null_mut(), Ordering::AcqRel);
+        if p.is_null() {
+            None
+        } else {
+            // SAFETY: p is Box<Waker>
+            let boxed = unsafe { Box::from_raw(p) };
+            Some(*boxed)
+        }
+    }
+
+    pub fn wake_and_clear(&self) {
+        if let Some(w) = self.take() {
+            w.wake();
+        }
+    }
+}
+
+impl Drop for WakerCell {
+    fn drop(&mut self) {
+        let p = self.ptr.load(Ordering::Acquire);
+        if !p.is_null() {
+            unsafe { drop(Box::from_raw(p)) };
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct AsyncInfo {
     pub handle: u64,
     pub result_values: Mutex<Box<[u64; 4]>>,
+    pub state: AtomicPromiseState,
+    pub waker: WakerCell,
+}
+
+#[atomic_enum]
+pub enum PromiseState {
+    None,
+    Waiting,
+    Completed,
+    TimedOut,
+    CancelPending
 }
