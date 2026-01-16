23a373fc15dbf20e18259c55fa4a77afae99cc9f
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2026-01-16 11:44:22 +0300

    8 byte aligned by default

--- src/windows/win_hv/src/nt/probe.rs
+++ src/windows/win_hv/src/nt/probe.rs
@@ -1,17 +1,17 @@
 use wdk_sys::ntddk::{ProbeForRead, ProbeForWrite};
 
-pub(crate) fn probe_for_write(address: *mut u8, length: usize, align: u32) -> Result<(), ()> {
+pub(crate) fn probe_for_write(address: *mut u8, length: usize) -> Result<(), microseh::Exception> {
     unsafe {
-        match microseh::try_seh(|| ProbeForWrite(address as _, length as _, align)) {
+        match microseh::try_seh(|| ProbeForWrite(address as _, length as _, 8)) {
             Ok(()) => Ok(()),
-            Err(_) => Err(()),
+            Err(x) => Err(x),
         }
     }
 }
 
-pub(crate) fn probe_for_read(address: *mut u8, length: usize, align: u32) -> Result<(), ()> {
+pub(crate) fn probe_for_read(address: *mut u8, length: usize) -> Result<(), ()> {
     unsafe {
-        match microseh::try_seh(|| ProbeForRead(address as _, length as _, align)) {
+        match microseh::try_seh(|| ProbeForRead(address as _, length as _, 8)) {
             Ok(()) => Ok(()),
             Err(_) => Err(()),
         }
