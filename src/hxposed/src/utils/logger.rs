8d220059a520225db29a64a62d12101f054b501d
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2026-03-19 15:44:22 -0700

    Overhaul
    Removed log dependency and human-readable format.
    HxPosed now logs in its own binary format for speed and simplicity

--- src/hxposed/src/utils/logger.rs
+++ src/hxposed/src/utils/logger.rs
@@ -1,117 +1,175 @@
-use crate::print;
-use alloc::string::ToString;
-use alloc::vec::Vec;
-use alloc::{format, vec};
-use com_logger::Serial;
-use core::cell::OnceCell;
-use core::fmt::Write;
-use log::{Metadata, Record};
-use spin::{Lazy, Mutex};
+use crate::win::{KeGetCurrentProcessorNumber, KeQuerySystemTime};
+use alloc::boxed::Box;
+use x86::io::outb;
 
-struct LogBuffer {
-    buffer: Vec<u8>,
+pub struct HvLogger {
+    buffer: Box<[LogEntry; 4096 * 8]>,
     cursor: usize,
 }
 
-impl LogBuffer {
-    pub fn new(capacity: usize) -> Self {
+impl HvLogger {
+    pub fn new() -> Self {
         Self {
-            buffer: vec![0; capacity],
+            buffer: unsafe { Box::new_zeroed().assume_init() },
             cursor: 0,
         }
     }
 
-    pub fn as_str(&self) -> &str {
-        str::from_utf8(&self.buffer[self.cursor..self.cursor]).unwrap_or("Invalid buffer.")
+    // <qemu:arg value="-chardev"/>
+    // <qemu:arg value="file,id=debuglog,path=/tmp/guest_debug.log,append=on"/>
+    // <qemu:arg value="-device"/>
+    // <qemu:arg value="isa-debugcon,iobase=0xe9,chardev=debuglog"/>
+    fn serial_out(entry: &LogEntry) {
+        // SAFETY: the struct is exactly 64 bytes and aligned
+        let ptr = entry as *const LogEntry as *const u8;
+
+        for i in 0..64 {
+            unsafe {
+                let byte = *ptr.add(i);
+                outb(0xe9, byte);
+            }
+        }
     }
 
-    pub fn rewind_and_clear(&mut self) {
-        self.buffer.fill(0);
-        self.cursor = 0;
+    pub fn dump_to_serial(&self) {
+        for i in 0..self.cursor {
+            Self::serial_out(&self.buffer[i]);
+        }
     }
-}
 
-impl Write for LogBuffer {
-    fn write_str(&mut self, s: &str) -> core::fmt::Result {
-        let remaining_len = self.buffer.len() - self.cursor;
-        let bytes = s.as_bytes();
-
-        if bytes.len() > remaining_len {
-            self.rewind_and_clear();
+    pub fn log(&mut self, log_type: LogType, event: LogEvent) {
+        if self.cursor >= self.buffer.len() {
+            self.cursor = 0;
         }
 
-        self.buffer[self.cursor..self.cursor + bytes.len()].copy_from_slice(bytes);
-        Ok(())
+        self.buffer[self.cursor] = LogEntry::new(log_type, event);
+        Self::serial_out(&self.buffer[self.cursor]);
+        self.cursor += 1;
+    }
+
+    pub fn trace(&mut self, event: LogEvent) {
+        self.log(LogType::Trace, event);
+    }
+
+    pub fn info(&mut self, event: LogEvent) {
+        self.log(LogType::Info, event);
+    }
+
+    pub fn warn(&mut self, event: LogEvent) {
+        self.log(LogType::Warn, event);
+    }
+
+    pub fn error(&mut self, event: LogEvent) {
+        self.log(LogType::Error, event);
     }
 }
 
-pub struct NtLogger {
-    log_buffer: Mutex<OnceCell<LogBuffer>>,
-    pub is_init: bool,
-    com: Lazy<Mutex<Serial>>,
+#[repr(C, align(8))]
+#[derive(Default, Copy, Clone)]
+pub struct LogEntry {
+    pub log_type: LogType,
+    pub processor: u64,
+    pub timestamp: u64,
+    pub discriminant: u64,
+    pub arg1: u64,
+    pub arg2: u64,
+    pub arg3: u64,
+    pub arg4: u64,
 }
+const _: () = assert!(size_of::<LogEntry>() == 64);
 
-unsafe impl Send for NtLogger {}
-unsafe impl Sync for NtLogger {}
-
-impl NtLogger {
-    pub const fn default() -> Self {
+impl LogEntry {
+    pub fn new(log_type: LogType, event: LogEvent) -> Self {
+        let raw = event.into_raw();
         Self {
-            log_buffer: Mutex::new(OnceCell::new()),
-            is_init: false,
-            com: Lazy::new(|| Mutex::new(Serial::new(0x3F8))),
+            log_type,
+            discriminant: raw.0,
+            arg1: raw.1,
+            arg2: raw.2,
+            arg3: raw.3,
+            arg4: raw.4,
+            processor: unsafe { KeGetCurrentProcessorNumber() as _ },
+            timestamp: unsafe { KeQuerySystemTime() },
         }
-    }
-
-    pub fn force_get_memory_buffer(&mut self) -> &str {
-        // SAFETY: this is executed during a panic. so its "safe" to assume we can force unlock it
-        // SAFET: Unless its absolutely init...
-        if !self.is_init {
-            return "Logger not initialized";
-        }
-
-        unsafe { self.log_buffer.force_unlock() };
-        let lock = self.log_buffer.get_mut();
-        lock.get_mut().unwrap().as_str()
-    }
-
-    pub fn init(&mut self) {
-        {
-            let lock = self.log_buffer.lock();
-            let _ = lock.set(LogBuffer::new(4096 * 10));
-        }
-        self.is_init = true;
     }
 }
 
-impl log::Log for NtLogger {
-    fn enabled(&self, _metadata: &Metadata) -> bool {
-        true
-    }
+#[repr(u64)]
+#[derive(Copy, Clone, Default)]
+pub enum LogEvent {
+    #[default]
+    None = 0,
+    VmxExitReason(u64),
+    RIP(u64, u64) = 2,
+    VCPU(u64) = 3,
+    UnknownExitReason(u64) = 4,
+    NoHxInfo = 5,
+    HyperCall(u64, u64, u64, u64) = 6,
+    HcDispatch(u64, u64) = 7,
+    CallError(u64, u64) = 8,
+    HyperResult(u64, u64, u64) = 9,
+    VirtualizingProcessor(u32) = 10,
+    ProcessorVirtualized(u32) = 11,
+    FailedToMap = 12,
+    DelayedStart = 13,
+    HxPosedInit(u64, u64) = 14,
+    NtVersion(u64) = 15,
+    WindowsVersion(u32, u32) = 16,
+    FailedToAllocate = 17,
+    Exception(u32) = 18,
+    Catastrophic(u64, u32, u32) = 19,
+    ProcessorReady(u32, u64) = 20,
+    LaunchingProcessor = 21,
+    Vmclear(u64, u64) = 22,
+    Vmptrld(u64, u64) = 23,
+    Vmxon(u64, u64) = 24,
+    Panic(u64, u64) = 25,
+    WritingAsyncBuffer(u64, u64) = 26,
+    WrittenAsyncBuffer(u64, u64) = 27,
+}
 
-    fn log(&self, record: &Record) {
-        let args = format!(
-            "{:>8}: {} ({}, {}:{})\n",
-            record.level(),
-            record.args(),
-            record.target(),
-            record.file().unwrap_or("<unknown>"),
-            record.line().unwrap_or(0),
-        )
-        .to_string();
-
-        /*{
-            let mut lock = self.serial.lock();
-            let _ = lock.get_mut().unwrap().write_str(args.as_str());
-        }*/
-        {
-            let mut lock = self.log_buffer.lock();
-            let _ = lock.get_mut().unwrap().write_str(args.as_str());
-            //let _ = self.com.lock().write_str(args.as_str());
+impl LogEvent {
+    // generally, in logging, goal is to save as much space as possible. but since rust enums are ass we have to do this
+    pub const fn into_raw(self) -> (u64, u64, u64, u64, u64) {
+        match self {
+            LogEvent::None => (0, 0, 0, 0, 0),
+            LogEvent::VmxExitReason(x) => (1, x, 0, 0, 0),
+            LogEvent::RIP(x, y) => (2, x, y, 0, 0),
+            LogEvent::VCPU(x) => (3, x, 0, 0, 0),
+            LogEvent::UnknownExitReason(x) => (4, x, 0, 0, 0),
+            LogEvent::NoHxInfo => (5, 0, 0, 0, 0),
+            LogEvent::HyperCall(x, y, z, q) => (6, x, y, z, q),
+            LogEvent::HcDispatch(x, y) => (7, x, y, 0, 0),
+            LogEvent::CallError(x, y) => (8, x, y, 0, 0),
+            LogEvent::HyperResult(x, y, z) => (9, x, y, z, 0),
+            LogEvent::VirtualizingProcessor(x) => (10, x as _, 0, 0, 0),
+            LogEvent::ProcessorVirtualized(x) => (11, x as _, 0, 0, 0),
+            LogEvent::FailedToMap => (12, 0, 0, 0, 0),
+            LogEvent::DelayedStart => (13, 0, 0, 0, 0),
+            LogEvent::HxPosedInit(x, y) => (14, x, y, 0, 0),
+            LogEvent::NtVersion(x) => (15, x, 0, 0, 0),
+            LogEvent::WindowsVersion(x, y) => (16, x as _, y as _, 0, 0),
+            LogEvent::FailedToAllocate => (17, 0, 0, 0, 0),
+            LogEvent::Exception(x) => (18, x as _, 0, 0, 0),
+            LogEvent::Catastrophic(x, y, z) => (19, x, y as _, z as _, 0),
+            LogEvent::ProcessorReady(x, y) => (20, x as _, y, 0, 0),
+            LogEvent::LaunchingProcessor => (21, 0, 0, 0, 0),
+            LogEvent::Vmclear(x, y) => (22, x, y, 0, 0),
+            LogEvent::Vmptrld(x, y) => (23, x, y, 0, 0),
+            LogEvent::Vmxon(x, y) => (24, x, y, 0, 0),
+            LogEvent::Panic(x, y) => (25, x, y, 0, 0),
+            LogEvent::WritingAsyncBuffer(x, y) => (26, x, y, 0, 0),
+            LogEvent::WrittenAsyncBuffer(x, y) => (27, x, y, 0, 0),
         }
-
-        print!("{}", args);
     }
+}
 
-    fn flush(&self) {}
+#[repr(u64)]
+#[derive(Copy, Clone, Default)]
+pub enum LogType {
+    #[default]
+    Trace = 0,
+    Info = 1,
+    Warn = 2,
+    Error = 3,
 }
