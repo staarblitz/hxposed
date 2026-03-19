4f28295fb7a5b58be7a078c648397f8b1418c457
Staarblitz <229130767+staarblitz@users.noreply.github.com>
2026-03-19 15:40:56 -0700

    Remove log

--- src/hxposed/src/hypervisor/init.rs
+++ src/hxposed/src/hypervisor/init.rs
@@ -1,11 +1,11 @@
 use crate::hypervisor::Hypervisor;
 use alloc::vec;
 
-static mut HYPERVISOR: Hypervisor = Hypervisor { cpus: vec![] };
+#[unsafe(no_mangle)]
+pub static mut HYPERVISOR: Hypervisor = Hypervisor { cpus: vec![] };
 
 #[allow(static_mut_refs)]
 pub(crate) fn init_hypervisor() {
-    log::info!("Initializing hypervisor");
     unsafe {
         HYPERVISOR.virtualize_system()
     }
